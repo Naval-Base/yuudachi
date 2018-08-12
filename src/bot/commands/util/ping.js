9a7abd75cdcbbf4a8798714ea1aad6ff56b3b83c
Crawl <icrawltogo@gmail.com>
2018-08-12 14:24:51 +0200

    chore: change ping output strings

--- src/bot/commands/util/ping.js
+++ src/bot/commands/util/ping.js
@@ -7,11 +7,11 @@ const RESPONSES = [
 	'Maybe later.',
 	stripIndents`:ping_pong: Pong! \`$(ping)ms\`
 		Heartbeat: \`$(heartbeat)ms\``,
-	stripIndents`I-it's not like I wanted to pong! \`$(ping)ms\`
+	stripIndents`Just so you know, I'm not doing this for fun! \`$(ping)ms\`
 		Doki doki: \`$(heartbeat)ms\``,
-	stripIndents`D-don't think this means anything special! \`$(ping)ms\`
+	stripIndents`Don't think this means anything special! \`$(ping)ms\`
 		Heartbeat: \`$(heartbeat)ms\``,
-	stripIndents`D-don't think this means anything special! \`$(ping)ms\`
+	stripIndents`Can we get on with this already?! \`$(ping)ms\`
 		Heartbeat: \`$(heartbeat)ms\``
 ];
 
