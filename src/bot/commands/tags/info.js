55b5259f2abd3162a63dedb0087858dca75ec886
Crawl <icrawltogo@gmail.com>
2018-08-13 15:23:34 +0200

    feat: add aliases to the info output

--- src/bot/commands/tags/info.js
+++ src/bot/commands/tags/info.js
@@ -32,11 +32,12 @@ class TagInfoCommand extends Command {
 		const guild = this.client.guilds.get(tag.guild);
 		const embed = new MessageEmbed()
 			.setColor(3447003)
-			.addField('User', user ? `${user.tag} (ID: ${user.id})` : "Couldn't fetch user.")
-			.addField('Guild', guild ? `${guild.name}` : "Couldn't fetch guild.")
-			.addField('Uses', tag.uses)
-			.addField('Created at', moment.utc(tag.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ'))
-			.addField('Modified at', moment.utc(tag.updatedAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ'));
+			.addField('❯ User', user ? `${user.tag} (ID: ${user.id})` : "Couldn't fetch user.")
+			.addField('❯ Guild', guild ? `${guild.name}` : "Couldn't fetch guild.")
+			.addField('❯ Aliases', tag.aliases.length ? tag.aliases.map(t => `\`${t}\``).sort().join(', ') : 'No aliases.')
+			.addField('❯ Uses', tag.uses)
+			.addField('❯ Created at', moment.utc(tag.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ'))
+			.addField('❯ Modified at', moment.utc(tag.updatedAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ'));
 
 		return message.util.send(embed);
 	}
