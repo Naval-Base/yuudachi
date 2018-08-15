cdde2fb0726faeffaf3ed049f8e84c7143ddc55a
Crawl <icrawltogo@gmail.com>
2018-08-15 03:00:23 +0200

    fix: remove console logs

--- src/bot/commands/github/issue-pr.js
+++ src/bot/commands/github/issue-pr.js
@@ -134,9 +134,7 @@ class GitHubPROrIssueCommand extends Command {
 				headers: { Authorization: `Bearer ${GITHUB_API_KEY}` },
 				body: JSON.stringify({ query })
 			});
-			console.log(res);
 			body = await res.json();
-			console.log(body);
 		} catch (error) {
 			return message.util.reply("Yukikaze couldn't find the requested information. Maybe look for something that actually exists the next time!");
 		}
