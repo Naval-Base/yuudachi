48edec342847887e2a2d9eea2bf0de75b1244639
Crawl <icrawltogo@gmail.com>
2018-08-18 21:05:17 +0200

    fix: make tag shortcut only be usable in guilds

--- src/bot/listeners/commandHandler/messageInvalid.js
+++ src/bot/listeners/commandHandler/messageInvalid.js
@@ -10,7 +10,7 @@ class MessageInvalidListener extends Listener {
 	}
 
 	async exec(message) {
-		if (message.util.parsed.prefix) {
+		if (message.guild && message.util.parsed.prefix) {
 			if (!message.util.parsed.alias || !message.util.parsed.afterPrefix) return;
 			const command = this.client.commandHandler.modules.get('tag-show');
 			return this.client.commandHandler.runCommand(message, command, await command.parse(message, message.util.parsed.afterPrefix));
