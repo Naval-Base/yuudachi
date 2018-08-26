c7906228c3fbf9f6497aa6b2518bd251b8f3ceb3
Crawl <icrawltogo@gmail.com>
2018-08-26 03:00:43 +0200

    chore: more breadcrumbs

--- src/bot/listeners/commandHandler/commandStarted.js
+++ src/bot/listeners/commandHandler/commandStarted.js
@@ -11,13 +11,37 @@ class CommandStartedListener extends Listener {
 	}
 
 	exec(message, command, args) {
+		Raven.captureBreadcrumb({
+			message: 'command_started',
+			category: command.category.id,
+			data: {
+				user: {
+					id: message.author.id,
+					username: message.author.tag
+				},
+				/* eslint-disable multiline-ternary */
+				guild: message.guild ? {
+					id: message.guild.id,
+					name: message.guild.name
+				} : null,
+				command: {
+					id: command.id,
+					aliases: command.aliases,
+					category: command.category.id
+				},
+				message: {
+					id: message.id,
+					content: message.content
+				},
+				args
+			}
+		});
 		Raven.setContext({
 			user: {
 				id: message.author.id,
 				username: message.author.tag
 			},
 			extra: {
-				/* eslint-disable multiline-ternary */
 				guild: message.guild ? {
 					id: message.guild.id,
 					name: message.guild.name
--- src/bot/listeners/commandHandler/error.js
+++ src/bot/listeners/commandHandler/error.js
@@ -12,6 +12,31 @@ class CommandErrorListener extends Listener {
 
 	exec(error, message, command) {
 		this.client.logger.error(error);
+		Raven.captureBreadcrumb({
+			message: 'command_errored',
+			category: command.category.id,
+			data: {
+				user: {
+					id: message.author.id,
+					username: message.author.tag
+				},
+				/* eslint-disable multiline-ternary */
+				guild: message.guild ? {
+					id: message.guild.id,
+					name: message.guild.name
+				} : null,
+				command: {
+					id: command.id,
+					aliases: command.aliases,
+					category: command.category.id
+				},
+				message: {
+					id: message.id,
+					content: message.content
+				}
+				/* eslint-enable multiline-ternary */
+			}
+		});
 		Raven.captureException(error);
 	}
 }
