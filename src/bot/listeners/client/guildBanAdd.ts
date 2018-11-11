2107873091af16f67383eadb9b8e0c9e8b610915
Crawl <icrawltogo@gmail.com>
2018-11-11 16:42:23 +0100

    fix: change member to user in guildBanAdd

--- src/bot/listeners/client/guildBanAdd.ts
+++ src/bot/listeners/client/guildBanAdd.ts
@@ -1,5 +1,5 @@
 import { Listener } from 'discord-akairo';
-import { Message, Guild, GuildMember, User, TextChannel } from 'discord.js';
+import { Message, Guild, User, TextChannel } from 'discord.js';
 import Util from '../../util';
 import { Case } from '../../models/Cases';
 
@@ -12,9 +12,9 @@ export default class GuildBanAddListener extends Listener {
 		});
 	}
 
-	public async exec(guild: Guild, member: GuildMember) {
+	public async exec(guild: Guild, user: User) {
 		if (!this.client.settings.get(guild, 'moderation', undefined)) return;
-		if (this.client.cachedCases.delete(`${guild.id}:${member.id}:BAN`)) return;
+		if (this.client.cachedCases.delete(`${guild.id}:${user.id}:BAN`)) return;
 		const totalCases = this.client.settings.get(guild, 'caseTotal', 0) + 1;
 		this.client.settings.set(guild, 'caseTotal', totalCases);
 		const modLogChannel = this.client.settings.get(guild, 'modLogChannel', undefined);
@@ -23,7 +23,7 @@ export default class GuildBanAddListener extends Listener {
 			// @ts-ignore
 			const prefix = this.client.commandHandler.prefix({ guild });
 			const reason = `Use \`${prefix}reason ${totalCases} <...reason>\` to set a reason for this case`;
-			const embed = Util.logEmbed({ member, action: 'Ban', caseNum: totalCases, reason }).setColor(Util.CONSTANTS.COLORS.BAN);
+			const embed = Util.logEmbed({ member: user, action: 'Ban', caseNum: totalCases, reason }).setColor(Util.CONSTANTS.COLORS.BAN);
 			modMessage = await (this.client.channels.get(modLogChannel) as TextChannel).send(embed) as Message;
 		}
 		const casesRepo = this.client.db.getRepository(Case);
@@ -31,8 +31,8 @@ export default class GuildBanAddListener extends Listener {
 		dbCase.guild = guild.id;
 		if (modMessage) dbCase.message = modMessage.id;
 		dbCase.case_id = totalCases;
-		dbCase.target_id = member.id;
-		dbCase.target_tag = member instanceof User ? member.tag : member.user.tag;
+		dbCase.target_id = user.id;
+		dbCase.target_tag = user.tag;
 		dbCase.action = Util.CONSTANTS.ACTIONS.BAN;
 		await casesRepo.save(dbCase);
 	}
