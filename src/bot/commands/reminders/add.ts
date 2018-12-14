91e8010e1bb05760cc4cfa8f581c8da0b16fc4a7
Crawl <icrawltogo@gmail.com>
2018-12-14 09:37:10 +0100

    fix: pass correct time

--- src/bot/commands/reminders/add.ts
+++ src/bot/commands/reminders/add.ts
@@ -73,7 +73,7 @@ export default class ReminderAddCommand extends Command {
 			reason,
 			trigger: message.url,
 			// @ts-ignore
-			triggers_at: time
+			triggers_at: Date.now() + time
 		});
 
 		return message.util!.reply(`I'll remind you in ${ms(time)}`);
