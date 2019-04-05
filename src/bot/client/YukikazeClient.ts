21f85f3a84b82285e22b95e48661fabb25b46a28
iCrawl <icrawltogo@gmail.com>
2019-04-05 09:57:18 +0200

    fix: argumentDefaults

--- src/bot/client/YukikazeClient.ts
+++ src/bot/client/YukikazeClient.ts
@@ -79,14 +79,17 @@ export default class YukikazeClient extends AkairoClient {
 		commandUtil: true,
 		commandUtilLifetime: 3e5,
 		defaultCooldown: 3000,
-		defaultPrompt: {
-			modifyStart: (_, str) => `${str}\n\nType \`cancel\` to cancel the command.`,
-			modifyRetry: (_, str) => `${str}\n\nType \`cancel\` to cancel the command.`,
-			timeout: 'Guess you took too long, the command has been cancelled.',
-			ended: "More than 3 tries and you still didn't quite get it. The command has been cancelled",
-			cancel: 'The command has been cancelled.',
-			retries: 3,
-			time: 30000
+		argumentDefaults: {
+			prompt: {
+				modifyStart: (_, str) => `${str}\n\nType \`cancel\` to cancel the command.`,
+				modifyRetry: (_, str) => `${str}\n\nType \`cancel\` to cancel the command.`,
+				timeout: 'Guess you took too long, the command has been cancelled.',
+				ended: "More than 3 tries and you still didn't quite get it. The command has been cancelled",
+				cancel: 'The command has been cancelled.',
+				retries: 3,
+				time: 30000
+			},
+			otherwise: ''
 		}
 	});
 
