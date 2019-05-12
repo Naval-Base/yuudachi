5d685b12cb835b03cb5f983ade9e81dc10edb4d4
iCrawl <icrawltogo@gmail.com>
2019-05-12 16:02:27 +0200

    fix: return flag

--- src/bot/commands/config/delete.ts
+++ src/bot/commands/config/delete.ts
@@ -36,6 +36,6 @@ export default class DeleteConfigCommand extends Command {
 			}
 		};
 
-		Flag.continue(key);
+		return Flag.continue(key);
 	}
 }
--- src/bot/commands/config/set.ts
+++ src/bot/commands/config/set.ts
@@ -37,6 +37,6 @@ export default class SetConfigCommand extends Command {
 			}
 		};
 
-		Flag.continue(key);
+		return Flag.continue(key);
 	}
 }
