917e94f6d9bcc696f36afccb0c93afe44143773e
iCrawl <icrawltogo@gmail.com>
2019-07-25 16:37:55 +0200

    fix: circular stringify

--- src/bot/listeners/commandHandler/commandStarted.ts
+++ src/bot/listeners/commandHandler/commandStarted.ts
@@ -12,8 +12,8 @@ export default class CommandStartedListener extends Listener {
 		});
 	}
 
-	public exec(message: Message, command: Command, args: any[]): void {
-		this.client.logger.info(`Started ${command.id} on ${message.guild ? `${message.guild.name} (${message.guild.id})` : 'DM'}${Object.keys(args).length ? ` with arguments ${JSON.stringify(args)}` : ''}`, { topic: TOPICS.DISCORD_AKAIRO, event: EVENTS.COMMAND_STARTED });
+	public exec(message: Message, command: Command, args: any): void {
+		this.client.logger.info(`Started ${command.id} on ${message.guild ? `${message.guild.name} (${message.guild.id})` : 'DM'}${Object.keys(args).length && !args.command ? ` with arguments ${JSON.stringify(args)}` : ''}`, { topic: TOPICS.DISCORD_AKAIRO, event: EVENTS.COMMAND_STARTED });
 		this.client.prometheus.commandCounter.inc();
 		addBreadcrumb({
 			message: 'command_started',
