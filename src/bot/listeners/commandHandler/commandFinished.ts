d6b9cec4b9519ad5685ddf4e80d273e34647e268
iCrawl <icrawltogo@gmail.com>
2019-07-28 01:56:19 +0200

    fix: circular structure logging and remove message block logging

--- src/bot/listeners/commandHandler/commandFinished.ts
+++ src/bot/listeners/commandHandler/commandFinished.ts
@@ -12,6 +12,6 @@ export default class CommandFinishedListener extends Listener {
 	}
 
 	public exec(message: Message, command: Command, args: any): void {
-		this.client.logger.info(`Finished ${command.id} on ${message.guild ? `${message.guild.name} (${message.guild.id})` : 'DM'}${Object.keys(args).length ? ` with arguments ${JSON.stringify(args)}` : ''}`, { topic: TOPICS.DISCORD_AKAIRO, event: EVENTS.COMMAND_FINISHED });
+		this.client.logger.info(`Finished ${command.id} on ${message.guild ? `${message.guild.name} (${message.guild.id})` : 'DM'}${Object.keys(args).length && !args.command ? ` with arguments ${JSON.stringify(args)}` : ''}`, { topic: TOPICS.DISCORD_AKAIRO, event: EVENTS.COMMAND_FINISHED });
 	}
 }
