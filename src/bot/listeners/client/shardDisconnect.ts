3dfa331ea1d34906d8b07eff73f7d0058a557c42
iCrawl <icrawltogo@gmail.com>
2019-09-02 19:45:59 +0200

    fix: remove past tense from events

--- src/bot/listeners/client/shardDisconnect.ts
+++ src/bot/listeners/client/shardDisconnect.ts
@@ -5,15 +5,15 @@ export default class ShardDisconnectListener extends Listener {
 	public constructor() {
 		super('shardDisconnected', {
 			emitter: 'client',
-			event: 'shardDisconnected',
+			event: 'shardDisconnect',
 			category: 'client'
 		});
 	}
 
 	public exec(event: any, id: number): void {
-		this.client.logger.warn(`Hmm, I have to hide the fact I was defeated... I'll let you go this time! (${event.code})`, { topic: TOPICS.DISCORD, event: `SHARD ${id} DISCONNECTED` });
+		this.client.logger.warn(`Hmm, I have to hide the fact I was defeated... I'll let you go this time! (${event.code})`, { topic: TOPICS.DISCORD, event: `SHARD ${id} DISCONNECT` });
 		this.client.promServer.close();
 		this.client.node.removeListener('message', this.client.nodeMessage);
-		this.client.logger.info(`Metrics server closed.`, { topic: TOPICS.METRICS, event: `SHARD ${id} DISCONNECTED` });
+		this.client.logger.info(`Metrics server closed.`, { topic: TOPICS.METRICS, event: `SHARD ${id} DISCONNECT` });
 	}
 }
---
+++ src/bot/listeners/client/shardResume.ts
@@ -0,0 +1,19 @@
+import { Listener } from 'discord-akairo';
+import { TOPICS } from '../../util/logger';
+
+export default class ShardResumeListener extends Listener {
+	public constructor() {
+		super('shardResume', {
+			emitter: 'client',
+			event: 'shardResume',
+			category: 'client'
+		});
+	}
+
+	public exec(id: number): void {
+		this.client.logger.info(`You made it out fine thanks to my luck! You ought to be thankful!`, { topic: TOPICS.DISCORD, event: `SHARD ${id} RESUME` });
+		this.client.promServer.listen(5500);
+		this.client.node.on('message', this.client.nodeMessage);
+		this.client.logger.info(`Metrics listening on 5500`, { topic: TOPICS.METRICS, event: `SHARD ${id} RESUME` });
+	}
+}
