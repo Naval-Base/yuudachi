d03977ea0e67ebc94bd0088f2b0aa4428a442491
1Computer1 <22125769+1Computer1@users.noreply.github.com>
2019-10-04 20:38:35 -0400

    fix: edit not taking template flags into account (#520)
    * Fix edit not taking template flags into account
    
    * Fix template not being passed
    
    * Fix spacing

--- src/bot/commands/tags/edit.ts
+++ src/bot/commands/tags/edit.ts
@@ -115,6 +115,12 @@ export default class TagEditCommand extends Command {
 			variables: vars,
 		});
 
-		return message.util!.reply(MESSAGES.COMMANDS.TAGS.EDIT.REPLY(tag.name, staffRole && (hoist || unhoist)));
+		return message.util!.reply(
+			MESSAGES.COMMANDS.TAGS.EDIT.REPLY(
+				tag.name,
+				staffRole && (hoist || unhoist),
+				staffRole && (template || untemplate),
+			),
+		);
 	}
 }
