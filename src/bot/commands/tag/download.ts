8af06c2ddbbb45a34b1099aba450aca527c327ab
almostSouji <timoqueezle@gmail.com>
2020-02-29 13:49:02 +0100

    feat(tags): export to yaml
    * add dependency: js-yaml
    * add dev-dependency: @types/js-yaml
    * add aliases, createdAt, updatedAt to exported values

--- src/bot/commands/tag/download.ts
+++ src/bot/commands/tag/download.ts
@@ -3,6 +3,7 @@ import { GuildMember, Message } from 'discord.js';
 import { MESSAGES, PRODUCTION } from '../../util/constants';
 import { GRAPHQL, graphQLClient } from '../../util/graphQL';
 import { Tags, TagsInsertInput } from '../../util/graphQLTypes';
+import { safeDump } from 'js-yaml';
 
 export default class TagDownloadCommand extends Command {
 	public constructor() {
@@ -32,21 +33,19 @@ export default class TagDownloadCommand extends Command {
 			query: member ? GRAPHQL.QUERY.TAGS_MEMBER : GRAPHQL.QUERY.TAGS,
 			variables: where,
 		});
-		let tags: Pick<Tags, 'content' | 'name' | 'hoisted' | 'user' | 'templated'>[];
+		let tags: Pick<
+			Tags,
+			'aliases' | 'content' | 'createdAt' | 'hoisted' | 'name' | 'templated' | 'updatedAt' | 'user'
+		>[];
 		if (PRODUCTION) tags = data.tags;
 		else tags = data.tagsStaging;
 		if (!tags.length) return;
-		const output = tags.reduce((out, t) => {
-			out += `Name: ${t.name}${t.templated ? ' [TEMPLATED]' : ''}\r\nUser: ${t.user}\r\nContent:\r\n${t.content.replace(
-				/\n/g,
-				'\r\n',
-			)}\r\n\r\n========================================\r\n\r\n`;
-			return out;
-		}, '');
-
 		return message.util?.send(MESSAGES.COMMANDS.TAGS.DOWNLOAD.REPLY, {
 			files: [
-				{ attachment: Buffer.from(output, 'utf8'), name: `${member ? `${member.displayName}s_tags` : 'all_tags'}.txt` },
+				{
+					attachment: Buffer.from(safeDump(tags), 'utf8'),
+					name: `${member ? `${member.displayName}s_tags` : 'all_tags'}.yaml`,
+				},
 			],
 		});
 	}
