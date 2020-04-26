66b35b4ffdab93bae0af6dd9b0b2dfc9521945df
Will Nelson <will@pleb.xyz>
2020-04-26 12:11:06 -0700

    Add settings manager tests

---
+++ packages/api/src/managers/SettingsManager.test.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata';
+
+import { container } from 'tsyringe';
+import postgres, { SQL } from 'postgres';
+import { kSQL } from '../tokens';
+import SettingsManager from './SettingsManager';
+
+jest.mock('postgres', () => jest.fn(() => jest.fn()));
+
+const mockedPostgres: jest.MockedFunction<SQL> = postgres() as any;
+
+container.register(kSQL, { useValue: mockedPostgres });
+
+test('gets settings', async () => {
+	mockedPostgres.mockResolvedValue([{ value: 'bar' }]);
+
+	const manager = container.resolve(SettingsManager);
+	const result = await manager.get('1234', 'foo');
+
+	expect(result).toBe('bar');
+	expect(mockedPostgres).toHaveBeenCalledTimes(1);
+	expect(mockedPostgres).toHaveBeenCalledWith([`
+			select value
+			from guild_settings
+			where guild_id = `,`
+				and key = `,''], '1234', 'foo');
+});
