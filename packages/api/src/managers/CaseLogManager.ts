a37cb3e0e64fbc2c5173cd0128113119518ba6a5
Will Nelson <will@pleb.xyz>
2020-06-12 16:44:11 -0700

    feat(api): Unit test the API (#731)

--- packages/api/src/managers/CaseLogManager.ts
+++ packages/api/src/managers/CaseLogManager.ts
@@ -1,11 +1,11 @@
 import Rest from '@spectacles/rest';
-import { User } from '@spectacles/types';
+import { Role } from '@spectacles/types';
 import { stripIndents } from 'common-tags';
 import { has } from 'lodash';
 import { inject, injectable } from 'tsyringe';
 import { SQL } from 'postgres';
-import { RawCase } from './CaseManager';
-import { SettingsKeys } from './SettingsManager';
+import { RawCase, CaseAction } from './CaseManager';
+import SettingsManager, { SettingsKeys } from './SettingsManager';
 import { kSQL } from '../tokens';
 
 @injectable()
@@ -14,21 +14,18 @@ export default class CaseLogManager {
 		@inject(kSQL)
 		public readonly sql: SQL,
 		public readonly rest: Rest,
+		public readonly settings: SettingsManager,
 	) {}
 
 	public async create(item: RawCase): Promise<void> {
-		const [setting] = await this.sql`
-			select value
-			from guild_settings
-			where guild_id = ${item.guild_id}
-				and key = ${SettingsKeys.MOD_LOG_CHANNEL_ID}`;
-		if (!has(setting, 'value')) throw new Error('no mod log channel configured');
+		const logChannelId = await this.settings.get(item.guild_id, SettingsKeys.MOD_LOG_CHANNEL_ID);
+		if (!logChannelId) throw new Error('no mod log channel configured');
 
-		const logChannelId = (setting as { value: string }).value;
 		const logMessage = await this.rest.post(
 			`/channels/${logChannelId}/messages`,
 			{
 				embed: {
+					title: `${item.mod_tag} (${item.mod_id})`,
 					description: await this.generateLogMessage(item, logChannelId),
 					footer: `Case ${item.case_id}`,
 					timestamp: Date.now(),
@@ -43,19 +40,18 @@ export default class CaseLogManager {
 				and guild_id = ${item.guild_id}`;
 	}
 
-	public update(item: Partial<RawCase>): Promise<void> {
-		throw new Error('Method not implemented.');
-	}
-	public delete(item: Partial<RawCase>): Promise<void> {
-		throw new Error('Method not implemented.');
-	}
-
 	protected async generateLogMessage(case_: RawCase, logChannelId: string): Promise<string> {
-		const user: User = await this.rest.get(`/users/${case_.target_id}`);
+		let action = CaseAction[case_.action];
+		if (case_.action === CaseAction.ROLE && case_.role_id) {
+			const roles: Role[] = await this.rest.get(`/guilds/${case_.guild_id}/roles`);
+			const role = roles.find(role => role.id === case_.role_id);
+
+			if (role) action += ` "${role.name}" (${case_.role_id})`;
+		}
 
 		let msg = stripIndents`
-			**Member:** ${user.username}#${user.discriminator} (${case_.target_id})
-			**Action:** ${case_.action}
+			**Member:** ${case_.target_tag} (${case_.target_id})
+			**Action:** ${action}
 		`;
 
 		if (case_.action_expiration) {
