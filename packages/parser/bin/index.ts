672057b9c241db8374a439acbad98d8d4bf5a383
Crawl <buechler.noel@outlook.com>
2020-06-14 21:41:03 +0200

    feat(*): eslint and prettier formatting (#743)

--- packages/parser/bin/index.ts
+++ packages/parser/bin/index.ts
@@ -8,24 +8,34 @@ import { Lexer, Parser, Tokens, Unordered } from 'lexure';
 const broker = new Amqp('gateway');
 const sql = postgres();
 
-(async () => {
+void (async () => {
 	await broker.connect('rabbitmq');
 	await broker.createQueue('COMMAND');
 	await broker.subscribe(['MESSAGE_CREATE']);
-	
-	for await (const [message, { ack }] of on(broker, 'MESSAGE_CREATE') as AsyncIterableIterator<[Message, AmqpResponseOptions]>) {
+
+	for await (const [message, { ack }] of on(broker, 'MESSAGE_CREATE') as AsyncIterableIterator<
+		[Message, AmqpResponseOptions]
+	>) {
 		ack();
-		const [data] = await sql`select settings ->> 'PREFIX' as prefix
+		const [data] = (await sql`select settings ->> 'PREFIX' as prefix
 			from settings
 			where guild_id = ${message.guild_id}
-				is not null;` as [{ prefix: string | null }];
+				is not null;`) as [{ prefix: string | null }];
 		const prefix = data.prefix ?? '?';
-		const lexer = new Lexer(message.content).setQuotes([['"', '"'], ['“', '”']]);
+		const lexer = new Lexer(message.content).setQuotes([
+			['"', '"'],
+			['“', '”'],
+		]);
 		const tokens = lexer.lex();
-		const command = Tokens.extractCommand(s => s.startsWith(prefix) ? prefix.length : null, tokens);
+		const command = Tokens.extractCommand((s) => (s.startsWith(prefix) ? prefix.length : null), tokens);
 		if (!command) continue;
 		const parser = new Parser(tokens).setUnorderedStrategy(Unordered.longShortStrategy());
 		const res = parser.parse();
-		broker.publish('COMMAND', { command, arguments: { ordered: [...res.ordered], flags: [...res.flags], options: [...res.options] }, tokens, message });
+		broker.publish('COMMAND', {
+			command,
+			arguments: { ordered: [...res.ordered], flags: [...res.flags], options: [...res.options] },
+			tokens,
+			message,
+		});
 	}
 })();
