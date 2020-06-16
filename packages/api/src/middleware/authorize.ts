99a99e2fdd47a847420700b35c1fd5349583b1a3
Crawl <buechler.noel@outlook.com>
2020-06-16 22:33:20 +0200

    feat(handler): Implement handler (#747)

--- packages/api/src/middleware/authorize.ts
+++ packages/api/src/middleware/authorize.ts
@@ -4,7 +4,9 @@ import { USER_ID_HEADER } from '../Constants';
 
 export default function authorize(req: Request, res: Response, next?: NextHandler) {
 	const userId = req.headers[USER_ID_HEADER] as string;
-	if (!userId) return next?.(badRequest(`missing "${USER_ID_HEADER}" header`));
+	if (!userId) {
+		return next?.(badRequest(`missing "${USER_ID_HEADER}" header`));
+	}
 
 	// TODO: verify user ID with authorization server
 	req.userId = userId;
---
+++ packages/handler/src/Command.test.ts
@@ -0,0 +1,46 @@
+import Command, { commandInfo } from './Command';
+import { sep } from 'path';
+
+test('invalid file name', () => {
+	expect(commandInfo('foo')).toBeNull();
+});
+
+test('non-js file', () => {
+	expect(commandInfo('post.rs')).toBeNull();
+});
+
+test('instance', () => {
+	class TestCommand implements Command {
+		public name = 'test';
+		public category = 'test';
+
+		// eslint-disable-next-line @typescript-eslint/no-empty-function
+		public execute(): void {}
+	}
+
+	const test = new TestCommand();
+
+	expect(test.name).toBe('test');
+	expect(test.category).toBe('test');
+});
+
+test('no category', () => {
+	const command = commandInfo('test.js');
+	expect(command).not.toBeNull();
+	expect(command!.name).toBe('test');
+	expect(command!.category).toBe('none');
+});
+
+test('category', () => {
+	const command = commandInfo(`test${sep}test2.js`);
+	expect(command).not.toBeNull();
+	expect(command!.name).toBe('test2');
+	expect(command!.category).toBe('test');
+});
+
+test('nested category', () => {
+	const command = commandInfo(`test${sep}test2${sep}test3.js`);
+	expect(command).not.toBeNull();
+	expect(command!.name).toBe('test3');
+	expect(command!.category).toBe('test2');
+});
