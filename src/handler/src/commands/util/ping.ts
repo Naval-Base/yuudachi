b9420376aabade5fc7a9141397cbf068fd888d76
Noel <buechler.noel@outlook.com>
2020-08-31 21:12:11 +0200

    feat(api,handler): Add i18n and tags (#766)

--- src/handler/src/commands/util/ping.ts
+++ src/handler/src/commands/util/ping.ts
@@ -1,19 +1,25 @@
 import { injectable } from 'tsyringe';
 import { Message } from '@spectacles/types';
+import { Args } from 'lexure';
 import Rest from '@yuudachi/rest';
+import i18next from 'i18next';
 
 import Command from '../../Command';
 
 @injectable()
 export default class implements Command {
-	public constructor(public readonly rest: Rest) {}
+	public constructor(private readonly rest: Rest) {}
 
-	public async execute(message: Message) {
-		const msg: Message = await this.rest.post(`/channels/${message.channel_id}/messages`, { content: 'Pinging...' });
+	public async execute(message: Message, _: Args, locale: string) {
+		const msg: Message = await this.rest.post(`/channels/${message.channel_id}/messages`, {
+			content: i18next.t('command.ping.execute.pre_ping', { lng: locale }),
+		});
 		void this.rest.patch(`/channels/${message.channel_id}/messages/${msg.id}`, {
-			content: `Don't think this means anything special! \`${
-				Date.parse(msg.edited_timestamp ?? msg.timestamp) - Date.parse(message.edited_timestamp ?? message.timestamp)
-			}ms\``,
+			content: i18next.t('command.ping.execute.post_ping', {
+				ping:
+					Date.parse(msg.edited_timestamp ?? msg.timestamp) - Date.parse(message.edited_timestamp ?? message.timestamp),
+				lng: locale,
+			}),
 		});
 	}
 }
