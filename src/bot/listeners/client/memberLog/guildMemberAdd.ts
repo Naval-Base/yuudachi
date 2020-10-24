711a571b444e729db9309490942a644cd904af2a
Souji <timoqueezle@gmail.com>
2020-10-24 20:11:18 +0200

    feat(logs): add account creation information (#779)
    Co-authored-by: Noel <buechler.noel@outlook.com>

--- src/bot/listeners/client/memberLog/guildMemberAdd.ts
+++ src/bot/listeners/client/memberLog/guildMemberAdd.ts
@@ -1,6 +1,31 @@
 import { Listener } from 'discord-akairo';
 import { GuildMember, MessageEmbed, TextChannel } from 'discord.js';
-import { COLORS, SETTINGS } from '../../../util/constants';
+import { SETTINGS, MAX_TRUST_ACCOUNT_AGE } from '../../../util/constants';
+import * as moment from 'moment';
+import { stripIndents } from 'common-tags';
+
+function colorFromDuration(duration: number) {
+	const percent = Math.min(duration / (MAX_TRUST_ACCOUNT_AGE / 100), 100);
+	let r;
+	let g;
+	let b = 0;
+
+	if (percent < 50) {
+		r = 255;
+		g = Math.round(5.1 * percent);
+	} else {
+		g = 255;
+		r = Math.round(510 - 5.1 * percent);
+	}
+
+	const tintFactor = 0.3;
+
+	r += (255 - r) * tintFactor;
+	g += (255 - g) * tintFactor;
+	b += (255 - b) * tintFactor;
+
+	return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+}
 
 export default class GuildMemberAddMemberLogListener extends Listener {
 	public constructor() {
@@ -14,15 +39,32 @@ export default class GuildMemberAddMemberLogListener extends Listener {
 	public async exec(member: GuildMember) {
 		const memberlog = this.client.settings.get(member.guild, SETTINGS.MEMBER_LOG);
 		if (memberlog) {
+			const sinceCreationFormatted = moment.utc(member.user.createdAt ?? 0).fromNow();
+			const creationFormatted = moment.utc(member.user.createdAt ?? 0).format();
+			const joinFormatted = moment.utc().format();
 			const embed = new MessageEmbed()
-				.setColor(COLORS.MEMBER_JOIN)
+				.setColor(colorFromDuration(Date.now() - member.user.createdTimestamp))
 				.setAuthor(`${member.user.tag} (${member.id})`, member.user.displayAvatarURL())
 				.setFooter('User joined')
 				.setTimestamp(new Date());
 
 			if (memberlog.MENTION) {
-				embed.setDescription(member);
+				embed.setDescription(
+					stripIndents`
+						• Profile: ${member}
+						• Created: ${sinceCreationFormatted} (${creationFormatted})
+						• Joined: ${joinFormatted}
+					`,
+				);
+			} else {
+				embed.setDescription(
+					stripIndents`
+						• Created: ${sinceCreationFormatted} (${creationFormatted})
+						• Joined: ${joinFormatted}
+					`,
+				);
 			}
+
 			return (this.client.channels.cache.get(memberlog.ID) as TextChannel).send(embed);
 		}
 	}
