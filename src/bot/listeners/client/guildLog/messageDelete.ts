00e68a04bc516f03e63fb9e04a203a419ae84f06
Souji <timoqueezle@gmail.com>
2020-11-03 10:46:11 +0100

    feat(logs): log messages without content, log multiple attachments (#785)

--- src/bot/listeners/client/guildLog/messageDelete.ts
+++ src/bot/listeners/client/guildLog/messageDelete.ts
@@ -14,21 +14,33 @@ export default class MessageDeleteGuildLogListener extends Listener {
 	public async exec(message: Message) {
 		if (message.author.bot) return;
 		if (!message.guild) return;
-		if (!message.content) return;
 		const guildLogs = this.client.settings.get(message.guild, SETTINGS.GUILD_LOG);
 		if (guildLogs) {
 			const webhook = this.client.webhooks.get(guildLogs);
 			if (!webhook) return;
-			const attachment = message.attachments.first();
 			const embed = new MessageEmbed()
 				.setColor(COLORS.MESSAGE_DELETE)
 				.setAuthor(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL())
 				.addField('❯ Channel', message.channel)
-				.addField('❯ Content', `${message.content.substring(0, 1020)}`)
-				.addField('❯ Context Link', `[Jump To](${message.url})`, true);
-			if (attachment) embed.addField('❯ Attachment(s)', attachment.proxyURL);
-			embed.setTimestamp(new Date());
-			embed.setFooter('Deleted');
+				.setTimestamp(new Date())
+				.setFooter('Deleted');
+
+			if (message.content) {
+				embed.addField('❯ Content', `${message.content.substring(0, 1020)}`);
+			}
+
+			if (message.attachments.size) {
+				embed.addField(
+					'❯ Attachment(s)',
+					`• ${message.attachments.map((attachment) => attachment.proxyURL).join('\n• ')}`,
+				);
+			}
+
+			if (!message.content && message.embeds.length) {
+				embed.addField('❯ Embeds', `${message.embeds.length}`);
+			}
+
+			embed.addField('❯ Context Link', `[Jump To](${message.url})`, true);
 
 			return webhook.send({
 				embeds: [embed],
