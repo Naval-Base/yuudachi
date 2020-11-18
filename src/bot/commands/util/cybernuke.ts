Naval-Base/yuudachi

commit 41ca3be87612ebce7ff092b54f93edba279f4466

feat(*): ux improvements (#792)

src/bot/commands/util/cybernuke.ts
@@ -1,7 +1,7 @@
 import { stripIndents } from 'common-tags';
 import { Command } from 'discord-akairo';
 import { GuildMember, Message, Permissions, MessageAttachment } from 'discord.js';
-import { MESSAGES, DATE_FORMAT_LOGFILE } from '../../util/constants';
+import { MESSAGES, DATE_FORMAT_LOGFILE, DATE_FORMAT_WITH_SECONDS } from '../../util/constants';
 import * as moment from 'moment';
 import { ms } from '@naval-base/ms';
 import { EVENTS, TOPICS } from '../../util/logger';
@@ -78,8 +78,9 @@ export default class LaunchCybernukeCommand extends Command {
 		const joinCutoff = Date.now() - join;
 		const ageCutoff = Date.now() - age;
 
-		const joinCutoffFormatted = moment.utc(joinCutoff).format('YYYY/MM/DD hh:mm:ss');
-		const ageCutoffFormatted = moment.utc(ageCutoff).format('YYYY/MM/DD hh:mm:ss');
+		const nowFormatted = moment.utc().format(DATE_FORMAT_WITH_SECONDS);
+		const joinCutoffFormatted = moment.utc(joinCutoff).format(DATE_FORMAT_WITH_SECONDS);
+		const ageCutoffFormatted = moment.utc(ageCutoff).format(DATE_FORMAT_WITH_SECONDS);
 
 		const members = fetchedMembers.filter(
 			(member) => (member.joinedTimestamp ?? 0) > joinCutoff && member.user.createdTimestamp > ageCutoff,
@@ -87,17 +88,20 @@ export default class LaunchCybernukeCommand extends Command {
 
 		if (!members.size) {
 			return message.util?.send(
-				MESSAGES.COMMANDS.UTIL.CYBERNUKE.FAIL.NO_MEMBERS(`${joinCutoffFormatted} (UTC)`, `${ageCutoffFormatted} (UTC)`),
+				`${MESSAGES.COMMANDS.UTIL.CYBERNUKE.FAIL.NO_MEMBERS}\n${MESSAGES.COMMANDS.UTIL.CYBERNUKE.PARAMETERS(
+					`\`${nowFormatted} (UTC)\``,
+					`\`${joinCutoffFormatted} (UTC)\``,
+					`\`${ageCutoffFormatted} (UTC)\``,
+				)}`,
 			);
 		}
 
 		await message.util?.send(
-			MESSAGES.COMMANDS.UTIL.CYBERNUKE.PROMPT.CONFIRMATION(
-				message.author,
-				members.size,
-				`${joinCutoffFormatted} (UTC)`,
-				`${ageCutoffFormatted} (UTC)`,
-			),
+			`${MESSAGES.COMMANDS.UTIL.CYBERNUKE.PROMPT.CONFIRMATION}\n${MESSAGES.COMMANDS.UTIL.CYBERNUKE.PARAMETERS(
+				`\`${nowFormatted} (UTC)\``,
+				`\`${joinCutoffFormatted} (UTC)\``,
+				`\`${ageCutoffFormatted} (UTC)\``,
+			)}`,
 		);
 
 		const filter = (m: Message) =>
@@ -177,9 +181,9 @@ export default class LaunchCybernukeCommand extends Command {
 				return null;
 			}
 
-			return message.util?.send(MESSAGES.COMMANDS.UTIL.CYBERNUKE.FAIL.CONFIRMATION);
+			return message.channel.send(MESSAGES.COMMANDS.UTIL.CYBERNUKE.FAIL.CONFIRMATION);
 		} catch {
-			message.util?.send(MESSAGES.COMMANDS.UTIL.CYBERNUKE.FAIL.TIMEOUT);
+			message.channel.send(MESSAGES.COMMANDS.UTIL.CYBERNUKE.FAIL.TIMEOUT);
 		}
 
 		return null;

src/bot/listeners/client/memberLog/guildMemberAdd.ts
@@ -53,8 +53,8 @@ export default class GuildMemberAddMemberLogListener extends Listener {
 				parts.push(`• Profile: ${member}`);
 			}
 
-			parts.push(`• Created: ${sinceCreationFormatted} (${creationFormatted})`);
-			parts.push(`• Joined: ${joinFormatted}`);
+			parts.push(`• Created: \`${creationFormatted}\` (${sinceCreationFormatted})`);
+			parts.push(`• Joined: \`${joinFormatted}\``);
 
 			embed.setDescription(parts.join('\n'));
 

src/bot/listeners/client/memberLog/guildMemberRemove.ts
@@ -31,10 +31,10 @@ export default class GuildMemberRemoveMemberLogListener extends Listener {
 			}
 
 			if (member.joinedTimestamp) {
-				parts.push(`• Joined: ${sinceJoinFormatted} (${joinFormatted})`);
+				parts.push(`• Joined: \`${joinFormatted}\` (${sinceJoinFormatted})`);
 			}
 
-			parts.push(`• Left: ${leaveFormatted}`);
+			parts.push(`• Left: \`${leaveFormatted}\``);
 
 			embed.setDescription(parts.join('\n'));
 

src/bot/commands/mod/multiban.ts
@@ -175,9 +175,9 @@ export default class MultiBanCommand extends Command {
 				}
 				return null;
 			}
-			return message.util?.send(MESSAGES.COMMANDS.MOD.MULTIBAN.FAIL.CONFIRMATION);
+			return message.channel.send(MESSAGES.COMMANDS.MOD.MULTIBAN.FAIL.CONFIRMATION);
 		} catch {
-			message.util?.send(MESSAGES.COMMANDS.MOD.MULTIBAN.FAIL.TIMEOUT);
+			message.channel.send(MESSAGES.COMMANDS.MOD.MULTIBAN.FAIL.TIMEOUT);
 		}
 	}
 }

src/bot/structures/CaseHandler.ts
@@ -201,7 +201,7 @@ export default class CaseHandler {
 
 	private logMessage({ member, action, duration, message, reason, channel, reference }: Log) {
 		let msg = stripIndents`
-			**Member:** ${member instanceof User ? member.tag : member.user.tag} (${member.id})
+			**Member:** \`${member instanceof User ? member.tag : member.user.tag}\` (${member.id})
 			**Action:** ${action}
 		`;
 		if (action === 'Mute' && duration) {

src/bot/util/constants.ts
@@ -18,9 +18,9 @@ export enum ACTIONS {
 
 export const MAX_TRUST_ACCOUNT_AGE = 1000 * 60 * 60 * 24 * 7 * 4;
 
-export const DATE_FORMAT_WITH_SECONDS = 'YYYY/MM/DD hh:mm:ss';
+export const DATE_FORMAT_WITH_SECONDS = 'YYYY/MM/DD HH:mm:ss';
 
-export const DATE_FORMAT_LOGFILE = 'YYYY-MM-DD hh-mm-ss';
+export const DATE_FORMAT_LOGFILE = 'YYYY-MM-DD_HH-mm-ss';
 
 export enum COLORS {
 	BAN = 16718080,
@@ -816,16 +816,17 @@ export const MESSAGES = {
 							`${author}, how old (in minutes) should a member's account be for the cybernuke to ignore them (account age)?`,
 						RETRY: (author: User | null) => `${author}, the minimum is \`6 seconds\`.`,
 					},
-					CONFIRMATION: (author: User | null, members: number, join: string, age: string) =>
-						`${author}, cybernuke is projected to hit \`${members}\` guild members. Proceed? (y/n)\nParameters:\n• Join Date after: ${join}\n• Account Creation after: ${age}`,
+					CONFIRMATION: (author: User | null, members: number) =>
+						`${author}, cybernuke is projected to hit \`${members}\` guild members. Proceed? (y/n)`,
 				},
 				REPORT: 'The requested report is ready!',
 				FAIL: {
-					NO_MEMBERS: (join: string, age: string) =>
-						`Command execution failed. This cybernuke will not hit anyone, you might want to re-adjust the parameters and try again!\nParameters:\n• Join Date after: ${join}\n• Account Creation after: ${age}`,
+					NO_MEMBERS: `Command execution failed. This cybernuke will not hit anyone, you might want to re-adjust the parameters and try again!`,
 					CONFIRMATION: 'Action cancelled.',
 					TIMEOUT: 'Action timed out.',
 				},
+				PARAMETERS: (now: string, join: string, age: string) =>
+					`Parameters:\n• Current time: ${now}\n• Join date after: ${join}\n• Account creation after: ${age}`,
 			},
 
 			EVAL: {

commit 61c8d8e85dbd9593b34028e06f90dbe08186bd9e

fix(cybernuke): regression from #792 (#793)

src/bot/commands/util/cybernuke.ts
@@ -97,7 +97,10 @@ export default class LaunchCybernukeCommand extends Command {
 		}
 
 		await message.util?.send(
-			`${MESSAGES.COMMANDS.UTIL.CYBERNUKE.PROMPT.CONFIRMATION}\n${MESSAGES.COMMANDS.UTIL.CYBERNUKE.PARAMETERS(
+			`${MESSAGES.COMMANDS.UTIL.CYBERNUKE.PROMPT.CONFIRMATION(
+				message.author,
+				members.size,
+			)}\n${MESSAGES.COMMANDS.UTIL.CYBERNUKE.PARAMETERS(
 				`\`${nowFormatted} (UTC)\``,
 				`\`${joinCutoffFormatted} (UTC)\``,
 				`\`${ageCutoffFormatted} (UTC)\``,

src/bot/listeners/client/memberLog/guildMemberAdd.ts
@@ -53,8 +53,8 @@ export default class GuildMemberAddMemberLogListener extends Listener {
 				parts.push(`• Profile: ${member}`);
 			}
 
-			parts.push(`• Created: \`${creationFormatted}\` (${sinceCreationFormatted})`);
-			parts.push(`• Joined: \`${joinFormatted}\``);
+			parts.push(`• Created: \`${creationFormatted} (UTC)\` (${sinceCreationFormatted})`);
+			parts.push(`• Joined: \`${joinFormatted} (UTC)\``);
 
 			embed.setDescription(parts.join('\n'));
 

src/bot/listeners/client/memberLog/guildMemberRemove.ts
@@ -31,10 +31,10 @@ export default class GuildMemberRemoveMemberLogListener extends Listener {
 			}
 
 			if (member.joinedTimestamp) {
-				parts.push(`• Joined: \`${joinFormatted}\` (${sinceJoinFormatted})`);
+				parts.push(`• Joined: \`${joinFormatted} (UTC)\` (${sinceJoinFormatted})`);
 			}
 
-			parts.push(`• Left: \`${leaveFormatted}\``);
+			parts.push(`• Left: \`${leaveFormatted} (UTC)\``);
 
 			embed.setDescription(parts.join('\n'));
 