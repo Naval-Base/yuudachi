a91cbce480359d204cc57c3eae727231178b418b
iCrawl <buechler.noel@outlook.com>
2020-11-18 15:50:11 +0100

    fix(docker): docker setup

--- migrations/1576828134921_mute_message/down.sql
+++ migrations/1576828134921_mute_message/down.sql
@@ -1,4 +1,2 @@
 ALTER TABLE "public"."cases" DROP COLUMN "mute_message";
-COMMENT ON COLUMN "public"."cases"."mute_message" IS E'null'
-ALTER TABLE "staging"."cases" DROP COLUMN "mute_message";
-COMMENT ON COLUMN "staging"."cases"."mute_message" IS E'null'
+COMMENT ON COLUMN "public"."cases"."mute_message" IS E'null';
--- migrations/1576828134921_mute_message/up.sql
+++ migrations/1576828134921_mute_message/up.sql
@@ -1,5 +1,3 @@
 
 ALTER TABLE "public"."cases" ADD COLUMN "mute_message" text NULL;
-COMMENT ON COLUMN "public"."cases"."mute_message" IS E'The id of the message around this mute'
-ALTER TABLE "staging"."cases" ADD COLUMN "mute_message" text NULL;
-COMMENT ON COLUMN "staging"."cases"."mute_message" IS E'The id of the message around this mute'
+COMMENT ON COLUMN "public"."cases"."mute_message" IS E'The id of the message around this mute';
