a2c4913359d53c1bc06abeb4e58ca07605917789
almostSouji <timoqueezle@gmail.com>
2020-11-22 03:35:01 +0100

    chore(history): remove obsolete staff check
    * covered by category and inhibitor

--- src/bot/commands/mod/history.ts
+++ src/bot/commands/mod/history.ts
@@ -1,6 +1,6 @@
 import { Argument, Command } from 'discord-akairo';
 import { GuildMember, Message, Permissions } from 'discord.js';
-import { MESSAGES, SETTINGS } from '../../util/constants';
+import { MESSAGES } from '../../util/constants';
 import UserInfoCommand from '../info/user';
 
 export default class HistoryCommand extends Command {
@@ -43,9 +43,6 @@ export default class HistoryCommand extends Command {
 		message: Message,
 		{ member, showProfile, showCases }: { member: GuildMember | string; showProfile: boolean; showCases: boolean },
 	) {
-		const staffRole = message.member?.roles.cache.has(this.client.settings.get(message.guild!, SETTINGS.MOD_ROLE));
-		if (!staffRole) return;
-
 		const userInfoCmd = this.handler.findCommand('user') as UserInfoCommand;
 
 		if (member instanceof GuildMember) {
