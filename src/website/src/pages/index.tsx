718a257f6e1913180f4f460700c03b34f263d1e8
Noel <buechler.noel@outlook.com>
2020-11-22 15:15:24 +0100

    feat(website): guilds view (#794)

--- src/website/src/pages/index.tsx
+++ src/website/src/pages/index.tsx
@@ -1,8 +1,8 @@
 import { Box, Text } from '@chakra-ui/react';
 
 const Index = () => (
-	<Box w="100%" h="100%" bg="gray.900">
-		<Text color="white">Main</Text>
+	<Box w="100%" h="100%">
+		<Text>Main</Text>
 	</Box>
 );
 
