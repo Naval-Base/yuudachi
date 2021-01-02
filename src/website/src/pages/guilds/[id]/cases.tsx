967b9ae814c47c81d5c2c46df7d32b444096a21b
iCrawl <buechler.noel@outlook.com>
2021-01-02 18:21:23 +0100

    feat(website): redirect to / if not logged in

--- src/website/src/pages/guilds/[id]/cases.tsx
+++ src/website/src/pages/guilds/[id]/cases.tsx
@@ -1,11 +1,24 @@
+import { useEffect } from 'react';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 import { Box } from '@chakra-ui/react';
 
 import GuildLayout from '~/components/GuildLayout';
 
 const GuildCases = dynamic(() => import('~/components/GuildCases'));
 
+import { useUserStore } from '~/store/index';
+
 const GuildCasesPage = () => {
+	const user = useUserStore();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (!user.loggedIn) {
+			void router.push('/');
+		}
+	}, [user.loggedIn, router]);
+
 	return (
 		<GuildLayout>
 			<Box mt={10} px={{ base: 5 }}>
