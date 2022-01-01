3b866fb108368f5ac7e82dfa87d435c8e09b4848
Souji <timoqueezle@gmail.com>
2022-01-01 15:15:54 +0100

    chore(constants): adapt thresholds and expire timers (#1027)

--- src/Constants.ts
+++ src/Constants.ts
@@ -5,12 +5,12 @@ export const enum JobType {
 }
 
 export const MAX_TRUST_ACCOUNT_AGE = 1000 * 60 * 60 * 24 * 7 * 4;
-export const SPAM_THRESHOLD = 5;
-export const SPAM_EXPIRE_SECONDS = 20;
+export const SPAM_THRESHOLD = 4;
+export const SPAM_EXPIRE_SECONDS = 30;
 export const MENTION_THRESHOLD = 10;
-export const MENTION_EXPIRE_SECONDS = 20;
+export const MENTION_EXPIRE_SECONDS = 60;
 export const SCAM_THRESHOLD = 3;
-export const SCAM_EXPIRE_SECONDS = 20;
+export const SCAM_EXPIRE_SECONDS = 5 * 60;
 
 export const DATE_FORMAT_LOGFILE = 'YYYY-MM-DD_HH-mm-ss';
 
