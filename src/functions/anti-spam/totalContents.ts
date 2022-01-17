655739b2a80cfaf1d6837bc23e0221efe191cb67
JPBM135 <jpedrobm0@gmail.com>
2022-01-17 20:55:17 -0300

    feat(fuzzyHash): Added an basic implementation of fuzzy hashing for the Anti Spam

--- src/functions/anti-spam/totalContents.ts
+++ src/functions/anti-spam/totalContents.ts
@@ -1,9 +1,10 @@
 import type { Redis } from 'ioredis';
 import { container } from 'tsyringe';
 import { createHash } from 'crypto';
+import { digest, similarity } from 'ctph.js';
 
 import { kRedis } from '../../tokens';
-import { SPAM_EXPIRE_SECONDS } from '../../Constants';
+import { SPAM_EXPIRE_SECONDS, SPAM_HASH_THRESHOLD } from '../../Constants';
 
 export function createContentHash(content: string) {
 	return createHash('md5').update(content.toLowerCase()).digest('hex');
@@ -12,9 +13,24 @@ export function createContentHash(content: string) {
 export async function totalContent(content: string, guildId: string, userId: string): Promise<number> {
 	const redis = container.resolve<Redis>(kRedis);
 
-	const contentHash = createContentHash(content);
-	const channelSpamKey = `guild:${guildId}:user:${userId}:contenthash:${contentHash}`;
-	const total = await redis.incr(channelSpamKey);
-	await redis.expire(channelSpamKey, SPAM_EXPIRE_SECONDS);
-	return total;
+	const contentHash = digest(content);
+
+	const channelLasthashKey = `guild:${guildId}:user:${userId}:lastHash`;
+	const channelSpamKey = `guild:${guildId}:user:${userId}:spamCount`;
+
+	const lastHash = await redis.get(channelLasthashKey);
+
+	if (!lastHash) await redis.set(channelLasthashKey, contentHash, 'EX', SPAM_EXPIRE_SECONDS);
+
+	if (
+		!lastHash ||
+		lastHash === contentHash ||
+		(lastHash ? similarity(lastHash, contentHash) > SPAM_HASH_THRESHOLD : false)
+	) {
+		const total = await redis.incr(channelSpamKey);
+		await redis.expire(channelSpamKey, SPAM_EXPIRE_SECONDS);
+		return total;
+	}
+
+	return (await redis.get(channelSpamKey)) ?? 0;
 }
