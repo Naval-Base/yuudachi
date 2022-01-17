3c0323aa2bc20b761277dcf4d56e50cbb3452243
JPBM135 <67063134+JPBM135@users.noreply.github.com>
2022-01-17 00:12:49 -0300

    Basic implementation of fuzzy hashing
    Use the 'ctph.js' to compute two hashes and compare both

--- src/functions/anti-spam/totalContents.ts
+++ src/functions/anti-spam/totalContents.ts
@@ -1,18 +1,28 @@
 import type { Redis } from 'ioredis';
 import { container } from 'tsyringe';
-import { createHash } from 'crypto';
+import { digest, similarity } from 'ctph.js';
 
 import { kRedis } from '../../tokens';
-import { SPAM_EXPIRE_SECONDS } from '../../Constants';
+import { SPAM_EXPIRE_SECONDS, SPAM_SIMILARITY_THRESHOLD } from '../../Constants';
 
 export async function totalContent(content: string, guildId: string, userId: string): Promise<number> {
 	const redis = container.resolve<Redis>(kRedis);
+	const contentHash = digest(content);
 
-	// TODO: fuzzy hashing to combat spam bots that slightly vary content
+	const channelSpamKey = `guild:${guildId}:user:${userId}:spamCount`;
+	const channelHashKey = `guild:${guildId}:user:${userId}:contenthash`;
+	
+	const lastHash = await redis.get(channelHashKey);
+	const similarity = lastHash ? similarity(contentHash, lastHash) > SPAM_SIMILARITY_THRESHOLD : false;
 
-	const contentHash = createHash('md5').update(content.toLowerCase()).digest('hex');
-	const channelSpamKey = `guild:${guildId}:user:${userId}:contenthash:${contentHash}`;
-	const total = await redis.incr(channelSpamKey);
-	await redis.expire(channelSpamKey, SPAM_EXPIRE_SECONDS);
-	return total;
+	if (!lastHash) await redis.set(channelHashKey, contentHash, 'EX', SPAM_EXPIRE_SECONDS);
+
+	if (similarity) {
+			const total = await redis.incr(channelSpamKey);
+			await redis.expire(channelSpamKey, SPAM_EXPIRE_SECONDS);
+			return total;
+	};
+	
+	return 0;
+
 }
