decc57c73df43c7604b3affe564800e64d6b3d79
almostSouji <timoqueezle@gmail.com>
2022-01-18 18:30:34 +0100

    fix(lockdown): handle deleted target channel

--- src/functions/lockdowns/deleteLockdown.ts
+++ src/functions/lockdowns/deleteLockdown.ts
@@ -8,25 +8,25 @@ export async function deleteLockdown(channelId: Snowflake) {
 	const client = container.resolve<Client<true>>(Client);
 	const sql = container.resolve<Sql<any>>(kSQL);
 
-	const channel = (await client.channels.fetch(channelId)) as GuildChannel;
-
-	const [channelOverwrites] = await sql<[{ overwrites: PermissionOverwrites[] }?]>`
-		select overwrites
-		from lockdowns
-		where channel_id = ${channel.id}`;
-
-	if (!channelOverwrites) {
-		return null;
-	}
-
 	try {
+		const channel = (await client.channels.fetch(channelId)) as GuildChannel;
+
+		const [channelOverwrites] = await sql<[{ overwrites: PermissionOverwrites[] }?]>`
+			select overwrites
+			from lockdowns
+			where channel_id = ${channel.id}`;
+
+		if (!channelOverwrites) {
+			return null;
+		}
+
 		await channel.permissionOverwrites.set(channelOverwrites.overwrites);
 	} finally {
 		await sql`
 			delete
 			from lockdowns
-			where channel_id = ${channel.id}`;
+			where channel_id = ${channelId}`;
 	}
 
-	return channel.id;
+	return channelId;
 }
