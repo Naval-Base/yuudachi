b68faf76047758afebb48ff8582c714768222d9c
almostSouji <timoqueezle@gmail.com>
2022-01-19 19:16:35 +0100

    feat: ws backoff handling

--- src/websocket/WebSocketConnection.ts
+++ src/websocket/WebSocketConnection.ts
@@ -12,6 +12,7 @@ export class WebSocketConnection {
 	private readonly url: string;
 	private readonly redis: Redis;
 	private readonly headers: { [key: string]: string } | undefined;
+	private tries = 0;
 	public constructor(url: string, headers: { [key: string]: string } | undefined, redis: Redis) {
 		this.url = url;
 		this.redis = redis;
@@ -29,15 +30,18 @@ export class WebSocketConnection {
 	}
 
 	private onOpen() {
-		logger.info(`Websoket connected to at ${this.url}`);
+		logger.info({
+			msg: `Websoket connected to ${this.url}`,
+			url: this.url,
+		});
+		this.tries = 0;
 	}
 
 	private onMessage(data: Buffer) {
 		const objData = JSON.parse(data.toString()) as ScamAPIData;
-		logger.debug({
-			loc: 'onMessage',
-			data,
-			objData,
+		logger.info({
+			msg: `Websocket message received: ${data.toString()}`,
+			url: this.url,
 		});
 
 		if (objData.type === 'add') {
@@ -46,7 +50,10 @@ export class WebSocketConnection {
 				.sadd('scamdomains', ...objData.domains)
 				.set('scamdomains:refresh', Date.now())
 				.exec();
-			logger.debug(`domains added: ${objData.domains.join(', ')}`);
+			logger.info({
+				msg: `WebSocket based update of domains (add): ${objData.domains.join(', ')}`,
+				url: this.url,
+			});
 		}
 
 		if (objData.type === 'delete') {
@@ -55,20 +62,36 @@ export class WebSocketConnection {
 				.srem('scamdomains', ...objData.domains)
 				.set('scamdomains:refresh', Date.now())
 				.exec();
-			logger.debug(`domains removed: ${objData.domains.join(', ')}`);
+			logger.info({
+				msg: `WebSocket based update of domains (remove): ${objData.domains.join(', ')}`,
+				url: this.url,
+			});
 		}
 	}
 
 	private onClose(code: number, reason: string) {
-		logger.debug({
-			loc: 'onClose',
+		const backOff = this.backOff();
+		this.tries += 1;
+
+		logger.info({
+			msg: `WebSocket closed with code ${code} and reason ${reason}. Attempting reconnect after ${String(
+				backOff,
+			)} seconds`,
+			url: this.url,
 			code,
 			reason,
+			attemptReconnectAfterSeconds: backOff,
 		});
 
-		this.connection = new WebSocket(this.url, {
-			headers: this.headers,
-		});
-		this.connect();
+		setTimeout(() => {
+			this.connection = new WebSocket(this.url, {
+				headers: this.headers,
+			});
+			this.connect();
+		}, this.backOff());
+	}
+
+	private backOff() {
+		return Math.min(Math.floor(Math.exp(this.tries)), 10 * 60) * 1000;
 	}
 }
