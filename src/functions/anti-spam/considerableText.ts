2968c1624296b309161301ce347c256a2a68f880
almostSouji <timoqueezle@gmail.com>
2022-06-11 10:09:10 +0200

    feat(antispam): only consider blocked, not flagged automod messages

---
+++ src/functions/anti-spam/considerableText.ts
@@ -0,0 +1,25 @@
+import type { Message } from 'discord.js';
+import { AUTOMOD_FLAG_INDICATOR_FIELD_NAME } from '../../Constants.js';
+
+export function considerableText(message: Message) {
+	if (message.author.bot || !message.inGuild()) {
+		return null;
+	}
+
+	// @ts-expect-error Automod message, not yet in types (no overlap)
+	if (message.type === 24) {
+		const logEmbed = message.embeds[0]!;
+		const isFlagged = logEmbed.fields?.some((field) => field.name === AUTOMOD_FLAG_INDICATOR_FIELD_NAME);
+		if (isFlagged) {
+			return null;
+		}
+
+		return logEmbed.description;
+	}
+
+	if (!message.content.length) {
+		return null;
+	}
+
+	return message.content;
+}
