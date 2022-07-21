ff3210e08b4bb51df9ded86d7cb29eed4f0b0cf5
JPBM135 <jpedrobm0@gmail.com>
2022-07-21 18:07:27 -0300

    chore: Invert filters chore: Add an default args for when the data args arent specified

--- src/commands/moderation/sub/anti-raid-nuke/modal.ts
+++ src/commands/moderation/sub/anti-raid-nuke/modal.ts
@@ -1,4 +1,4 @@
-import type { CommandInteraction, InteractionResponse } from 'discord.js';
+import type { CommandInteraction, TextChannel } from 'discord.js';
 import type { Redis } from 'ioredis';
 
 interface AntiRaidModalArgs {
@@ -14,9 +14,14 @@ export async function modal(
 	locale: string,
 	redis: Redis,
 ): Promise<void> {
-	const { memberOnly, reason, days } = {
+	// @ts-expect-error
+	const parsedData = {
 		memberOnly: data.member_only ?? false,
 		reason: data.reason ?? null,
 		days: data.days ?? 1,
+		interaction,
+		logChannel,
+		locale,
+		redis,
 	};
 }
