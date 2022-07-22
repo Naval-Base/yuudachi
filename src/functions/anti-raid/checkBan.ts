54c683828e99512cebc159876d519bd1ff33213a
JPBM135 <jpedrobm0@gmail.com>
2022-07-22 19:01:10 -0300

    Pretty the prettier

--- src/functions/anti-raid/checkBan.ts
+++ src/functions/anti-raid/checkBan.ts
@@ -5,14 +5,17 @@ export function checkBan(member: GuildMember, authorId: string, modRoleId: strin
 	if (!member.bannable) return 'reject_unbannable';
 	if (member.user.bot) return 'reject_bot';
 	if (member.roles.cache.has(modRoleId)) return 'reject_mod';
-	if (member.permissions.any([
-		PermissionFlagsBits.Administrator,
-		PermissionFlagsBits.BanMembers,
-		PermissionFlagsBits.KickMembers,
-		PermissionFlagsBits.ModerateMembers,
-		PermissionFlagsBits.ManageChannels,
-		PermissionFlagsBits.ManageEvents,
-		PermissionFlagsBits.ManageGuild,
-	])) return 'reject_perms';
+	if (
+		member.permissions.any([
+			PermissionFlagsBits.Administrator,
+			PermissionFlagsBits.BanMembers,
+			PermissionFlagsBits.KickMembers,
+			PermissionFlagsBits.ModerateMembers,
+			PermissionFlagsBits.ManageChannels,
+			PermissionFlagsBits.ManageEvents,
+			PermissionFlagsBits.ManageGuild,
+		])
+	)
+		return 'reject_perms';
 	return null;
 }
