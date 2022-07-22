202244a86db50a7de61a7d9585d8d19954c52f3d
JPBM135 <jpedrobm0@gmail.com>
2022-07-22 03:47:47 -0300

    feat: function to check if a member is bulk bannable

---
+++ src/functions/anti-raid/checkBan.ts
@@ -0,0 +1,18 @@
+import { GuildMember, PermissionFlagsBits } from 'discord.js';
+
+export function checkBan(member: GuildMember, authorId: string, modRoleId: string): string | null {
+	if (member.id === authorId) return 'reject_self';
+	if (!member.bannable) return 'reject_unbannable';
+	if (member.user.bot) return 'reject_bot';
+	if (member.roles.cache.has(modRoleId)) return 'reject_mod';
+	if (member.permissions.any([
+		PermissionFlagsBits.Administrator,
+		PermissionFlagsBits.BanMembers,
+		PermissionFlagsBits.KickMembers,
+		PermissionFlagsBits.ModerateMembers,
+		PermissionFlagsBits.ManageChannels,
+		PermissionFlagsBits.ManageEvents,
+		PermissionFlagsBits.ManageGuild,
+	])) return 'reject_perms';
+	return null;
+}
