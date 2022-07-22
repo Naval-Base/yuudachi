e3ec6af9a74eb714879493763e0aa06eabee1dd1
JPBM135 <jpedrobm0@gmail.com>
2022-07-22 03:46:09 -0300

    fix: Allow the creation parameter to be optional

--- src/functions/logging/generateAntiRaidNukeCaseLog.ts
+++ src/functions/logging/generateAntiRaidNukeCaseLog.ts
@@ -3,13 +3,18 @@ import i18next from 'i18next';
 import type { Case } from '../cases/createCase.js';
 
 export function generateAntiRaidNukeCaseLog(cases: Case[], reason: string, locale: string) {
-	const msg = i18next.t('log.mod_log.anti_raid_nuke.description', {
+	const msg = [i18next.t('log.mod_log.anti_raid_nuke.description', {
 		reason,
 		members: cases.length,
-		joined_after: Formatters.time(new Date(cases[0]!.joinCutoff!), Formatters.TimestampStyles.LongDateTime),
-		account_created_after: Formatters.time(new Date(cases[0]!.accountCutoff!), Formatters.TimestampStyles.LongDateTime),
 		lng: locale,
-	});
+	})];
 
-	return msg;
+	if (cases[0]?.joinCutoff && cases[0]?.accountCutoff) {
+		msg.push(i18next.t('log.mod_log.anti_raid_nuke.parameters.cutoff', {
+			joined_after: Formatters.time(new Date(cases[0]!.joinCutoff), Formatters.TimestampStyles.LongDateTime),
+			account_created_after: Formatters.time(new Date(cases[0]!.accountCutoff), Formatters.TimestampStyles.LongDateTime),	
+		}));
+	}
+
+	return msg.join('\n');
 }
