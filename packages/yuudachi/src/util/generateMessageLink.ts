febbf73833545ddbeacfe87b536ff09898c1e570
almostSouji <timoqueezle@gmail.com>
2022-07-31 22:25:35 +0200

    chore: replace message link generator with native djs method

--- packages/yuudachi/src/util/generateMessageLink.ts
+++
@@ -1,5 +0,0 @@
-import type { Snowflake } from 'discord.js';
-
-export function generateMessageLink(guildId: Snowflake, channelId: Snowflake, messageId: Snowflake) {
-	return `https://discord.com/channels/${guildId}/${channelId}/${messageId}`;
-}
