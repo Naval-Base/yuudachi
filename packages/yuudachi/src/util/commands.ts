f7da5b8901e119f4d71089a05d666452d5150ed5
JPBM135 <jpedrobm0@gmail.com>
2022-08-01 20:56:41 -0300

    chore: Change from `Map` to `Collection` to support `.find()`

--- packages/yuudachi/src/util/commands.ts
+++ packages/yuudachi/src/util/commands.ts
@@ -1,9 +1,10 @@
+import { Collection } from 'discord.js';
 import { container } from 'tsyringe';
 import type { Command } from '../Command.js';
 import { kCommands } from '../tokens.js';
 
 export function createCommands() {
-	const commands = new Map<string, Command>();
+	const commands = new Collection<string, Command>();
 	container.register(kCommands, { useValue: commands });
 
 	return commands;
