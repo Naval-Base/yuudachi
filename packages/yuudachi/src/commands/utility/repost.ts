318ef3bd041b0ed059048c6aee1dc49ae1aae6a7
almostSouji <timoqueezle@gmail.com>
2022-08-07 00:45:40 +0200

    feat: centralize message parsing and visualizing methods

--- packages/yuudachi/src/commands/utility/repost.ts
+++ packages/yuudachi/src/commands/utility/repost.ts
@@ -1,23 +1,30 @@
+import type { Message } from 'discord.js';
 import i18next from 'i18next';
 import { type ArgsParam, Command, type InteractionParam, type LocaleParam, type CommandMethod } from '../../Command.js';
+import { formatMessageToEmbed } from '../../functions/logging/formatMessageToEmbed.js';
 import type { RepostCommand, RepostMessageContextCommand } from '../../interactions/index.js';
-import { addFields, truncateEmbed } from '../../util/embed.js';
+import { parseMessageLink, resolveMessage } from '../../util/parseMessages.js';
 
 export default class extends Command<typeof RepostCommand | typeof RepostMessageContextCommand> {
+	private async handle(
+		interaction: InteractionParam | InteractionParam<CommandMethod.MessageContext>,
+		message: Message<true>,
+		locale: string,
+	) {
+		await interaction.editReply({ embeds: [formatMessageToEmbed(message, locale)] });
+	}
+
 	public override async chatInput(
 		interaction: InteractionParam,
 		args: ArgsParam<typeof RepostCommand>,
 		locale: LocaleParam,
 	): Promise<void> {
 		await interaction.deferReply();
+		const parsedLink = parseMessageLink(args.message_link);
 
-		const linkRegex =
-			/(?:https?:\/\/(?:ptb\.|canary\.)?discord\.com\/channels\/(?<guildId>\d{17,20})\/(?<channelId>\d{17,20})\/(?<messageId>\d{17,20}))/gi;
-		const groups = args.message_link.match(linkRegex)?.groups;
-
-		if (!groups) {
+		if (!parsedLink) {
 			throw new Error(
-				i18next.t('command.utility.repost.errors.not_message_link', {
+				i18next.t('command.common.errors.not_message_link', {
 					val: args.message_link,
 					arg: 'message_link',
 					lng: locale,
@@ -25,71 +32,18 @@ export default class extends Command<typeof RepostCommand | typeof RepostMessage
 			);
 		}
 
-		const { guildId, channelId, messageId } = groups;
-		const guild = interaction.client.guilds.cache.get(guildId!);
+		const { guildId, channelId, messageId } = parsedLink;
+		const message = await resolveMessage(interaction.client, guildId!, channelId!, messageId!, locale);
 
-		if (!guild) {
-			throw new Error(
-				i18next.t('command.utility.repost.errors.no_guild', {
-					guild_id: guildId,
-					lng: locale,
-				}),
-			);
-		}
-
-		const channel = guild.channels.cache.get(channelId!);
-
-		if (!channel?.isTextBased()) {
-			throw new Error(
-				i18next.t('command.utility.repost.errors.no_channel', {
-					channel_id: channelId,
-					guild: guild.name,
-					lng: locale,
-				}),
-			);
-		}
-
-		const message = await channel.messages.fetch(messageId!).catch(() => {
-			throw new Error(
-				i18next.t('command.common.errors.no_message', {
-					message_id: messageId!,
-					// eslint-disable-next-line @typescript-eslint/no-base-to-string
-					channel: channel.toString(),
-					lng: locale,
-				}),
-			);
-		});
-
-		const author = message.author;
-		const embed = addFields({
-			author: { name: `${author.tag} (${author.id})`, icon_url: author.displayAvatarURL() },
-			description: message.content,
-			footer: {
-				// eslint-disable-next-line @typescript-eslint/no-base-to-string
-				text: message.channel.toString(),
-			},
-			timestamp: message.createdAt.toISOString(),
-		});
-
-		await interaction.editReply({ embeds: [truncateEmbed(embed)] });
+		await this.handle(interaction, message as Message<true>, locale);
 	}
 
 	public override async messageContext(
 		interaction: InteractionParam<CommandMethod.MessageContext>,
 		args: ArgsParam<typeof RepostMessageContextCommand>,
-		_: LocaleParam,
+		locale: LocaleParam,
 	): Promise<void> {
-		const author = args.message.author;
-		const embed = addFields({
-			author: { name: `${author.tag} (${author.id})`, icon_url: author.displayAvatarURL() },
-			description: args.message.content,
-			footer: {
-				// eslint-disable-next-line @typescript-eslint/no-base-to-string
-				text: args.message.channel.toString(),
-			},
-			timestamp: args.message.createdAt.toISOString(),
-		});
-
-		await interaction.reply({ embeds: [truncateEmbed(embed)] });
+		await interaction.deferReply();
+		await this.handle(interaction, args.message, locale);
 	}
 }
