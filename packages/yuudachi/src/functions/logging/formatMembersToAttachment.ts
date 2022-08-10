3fda518389fe49c3f2dfa105cf9497d9c167317c
JPBM135 <jpedrobm0@gmail.com>
2022-08-10 00:13:33 -0300

    chore: Date with seconds fix: success to true by default

--- packages/yuudachi/src/functions/logging/formatMembersToAttachment.ts
+++ packages/yuudachi/src/functions/logging/formatMembersToAttachment.ts
@@ -1,18 +1,18 @@
 import dayjs from 'dayjs';
 import type { Collection, GuildMember, Snowflake } from 'discord.js';
 import i18next from 'i18next';
-import { DATE_FORMAT_LOGFILE } from '../../Constants.js';
+import { DATE_FORMAT_WITH_SECONDS } from '../../Constants.js';
 import { colorBasedOnBoolean, colorBasedOnDifference } from '../../util/colors.js';
 import type { AntiRaidNukeResult } from '../anti-raid/blastOff.js';
 
-export function formatMemberSummary(member: GuildMember, locale: string, success = false) {
+export function formatMemberSummary(member: GuildMember, locale: string, success = true) {
 	const memberAge = Date.now() - member.user.createdTimestamp;
 	const joinAge = Date.now() - member.joinedTimestamp!;
 
-	return i18next.t('log.common.formatting.guildmember', {
+	return i18next.t('formatters.formatting.guildmember', {
 		id: colorBasedOnBoolean(success, member.user.id.padEnd(19, ' ')),
-		join: colorBasedOnDifference(joinAge, dayjs(member.joinedTimestamp).format(DATE_FORMAT_LOGFILE)),
-		creation: colorBasedOnDifference(memberAge, dayjs(member.user.createdTimestamp).format(DATE_FORMAT_LOGFILE)),
+		join: colorBasedOnDifference(joinAge, dayjs(member.joinedTimestamp).format(DATE_FORMAT_WITH_SECONDS)),
+		creation: colorBasedOnDifference(memberAge, dayjs(member.user.createdTimestamp).format(DATE_FORMAT_WITH_SECONDS)),
 		tag: member.user.tag,
 		lng: locale,
 	});
