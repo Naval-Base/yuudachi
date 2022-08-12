5cf97898d8c86093b472f884eeda2f1d403de8f9
iCrawl <buechler.noel@outlook.com>
2022-08-12 23:38:18 +0200

    refactor: proper job handling

--- packages/yuudachi/src/jobs.ts
+++ packages/yuudachi/src/jobs.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url';
+import Bree from 'bree';
 import { Client, type Snowflake } from 'discord.js';
 import { container } from 'tsyringe';
 import { JobType } from './Constants.js';
@@ -6,73 +7,77 @@ import { deleteCase } from './functions/cases/deleteCase.js';
 import { deleteLockdown } from './functions/lockdowns/deleteLockdown.js';
 import { upsertCaseLog } from './functions/logging/upsertCaseLog.js';
 import { logger } from './logger.js';
-import { createBree } from './util/bree.js';
-
-const bree = createBree();
+import { kBree } from './tokens.js';
 
 export async function registerJobs() {
 	const client = container.resolve<Client<true>>(Client);
 
-	bree.on('worker created', (name: string) => {
-		bree.workers.get(name)?.on(
-			'message',
-			// eslint-disable-next-line @typescript-eslint/no-misused-promises
-			async (message: string | { op: JobType; d: { guildId: Snowflake; channelId: Snowflake; caseId: number } }) => {
-				if (typeof message !== 'string') {
-					switch (message.op) {
-						case JobType.Case: {
-							try {
-								const guild = client.guilds.resolve(message.d.guildId);
-								if (!guild) {
-									return;
-								}
-								const case_ = await deleteCase({ guild, user: client.user, caseId: message.d.caseId });
-								await upsertCaseLog(guild, client.user, case_);
-							} catch (e) {
-								const error = e as Error;
-								logger.error(error, error.message);
+	const bree = new Bree({
+		root: false,
+		logger: false,
+		// eslint-disable-next-line @typescript-eslint/no-misused-promises
+		workerMessageHandler: async (
+			message: string | { op: JobType; d: { guildId: Snowflake; channelId: Snowflake; caseId: number } },
+		) => {
+			if (typeof message !== 'string') {
+				switch (message.op) {
+					case JobType.Case: {
+						try {
+							const guild = client.guilds.resolve(message.d.guildId);
+							if (!guild) {
+								return;
 							}
-							return;
+							const case_ = await deleteCase({ guild, user: client.user, caseId: message.d.caseId });
+							await upsertCaseLog(guild, client.user, case_);
+						} catch (e) {
+							const error = e as Error;
+							logger.error(error, error.message);
 						}
-
-						case JobType.Lockdown: {
-							try {
-								await deleteLockdown(message.d.channelId);
-							} catch (e) {
-								const error = e as Error;
-								logger.error(error, error.message);
-							}
-							break;
-						}
-
-						default:
-							break;
+						return;
 					}
+
+					case JobType.Lockdown: {
+						try {
+							await deleteLockdown(message.d.channelId);
+						} catch (e) {
+							const error = e as Error;
+							logger.error(error, error.message);
+						}
+						break;
+					}
+
+					default:
+						break;
 				}
-			},
-		);
+			}
+		},
+	});
+	container.register(kBree, { useValue: bree });
+
+	bree.on('worker created', (name: string) => {
+		logger.info({ job: { name: name } }, `Running job: ${name}`);
 	});
 
 	bree.on('worker deleted', (name: string) => {
-		bree.workers.get(name)?.removeAllListeners();
+		logger.info({ job: { name: name } }, `Finished job: ${name}`);
 	});
 
 	try {
-		logger.info({ job: { name: 'modActionTimers' } }, `Registering job: modActionTimers`);
+		logger.info({ job: { name: 'modActionTimers' } }, 'Registering job: modActionTimers');
 		await bree.add({
 			name: 'modActionTimers',
 			interval: '1m',
 			path: fileURLToPath(new URL('./jobs/modActionTimers.js', import.meta.url)),
 		});
-		logger.info({ job: { name: 'modActionTimers' }, jobs: bree.config.jobs }, `Registered job: modActionTimers`);
+		logger.info({ job: { name: 'modActionTimers' } }, 'Registered job: modActionTimers');
 
-		logger.info({ job: { name: 'modLockdownTimers' } }, `Registering job: modLockdownTimers`);
+		logger.info({ job: { name: 'modLockdownTimers' } }, 'Registering job: modLockdownTimers');
 		await bree.add({
 			name: 'modLockdownTimers',
 			interval: '1m',
 			path: fileURLToPath(new URL('./jobs/modLockdownTimers.js', import.meta.url)),
 		});
-		logger.info({ job: { name: 'modLockdownTimers' }, jobs: bree.config.jobs }, `Registered job: modLockdownTimers`);
+		logger.info({ job: { name: 'modLockdownTimers' } }, 'Registered job: modLockdownTimers');
 
 		logger.info({ job: { name: 'scamDomainUpdateTimers' } }, 'Registering job: scamDomainUpdateTimers');
 		await bree.add({
@@ -81,10 +86,7 @@ export async function registerJobs() {
 			timeout: 0,
 			path: fileURLToPath(new URL('./jobs/scamDomainUpdateTimers.js', import.meta.url)),
 		});
-		logger.info(
-			{ job: { name: 'scamDomainUpdateTimers' }, jobs: bree.config.jobs },
-			'Registered job: scamDomainUpdateTimers',
-		);
+		logger.info({ job: { name: 'scamDomainUpdateTimers' } }, 'Registered job: scamDomainUpdateTimers');
 
 		await bree.start();
 	} catch (e) {
