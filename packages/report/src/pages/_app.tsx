e9a3303273cec138104c127d2688bb4c1139c621
iCrawl <buechler.noel@outlook.com>
2022-08-12 19:23:40 +0200

    fix: pull webfonts from cdn

--- packages/report/src/pages/_app.tsx
+++ packages/report/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '@unocss/reset/normalize.css';
+import '../styles/unocss.css';
 import '../styles/main.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
