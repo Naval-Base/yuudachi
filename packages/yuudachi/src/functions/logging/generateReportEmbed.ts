3b99bebdbc1f31f88eb3f8de3a76e461a35f3e4b
JPBM135 <jpedrobm0@gmail.com>
2022-08-15 14:46:15 -0300

    chore: change description to it's own file

--- packages/yuudachi/src/functions/logging/generateReportEmbed.ts
+++ packages/yuudachi/src/functions/logging/generateReportEmbed.ts
@@ -1,22 +1,8 @@
-import dayjs from 'dayjs';
-import {
-	type APIEmbed,
-	type Message,
-	type User,
-	time,
-	TimestampStyles,
-	hyperlink,
-	codeBlock,
-	messageLink,
-	type Snowflake,
-} from 'discord.js';
+import type { APIEmbed, Message, User } from 'discord.js';
 import i18next from 'i18next';
-import type { Sql } from 'postgres';
-import { container } from 'tsyringe';
+import { generateReportLog } from './generateReportLog.js';
 import { Color } from '../../Constants.js';
-import { kSQL } from '../../tokens.js';
 import { type Report, ReportStatus } from '../reports/createReport.js';
-import { getGuildSetting, SettingsKeys } from '../settings/getGuildSetting.js';
 
 function statusToColor(status: ReportStatus): Color {
 	switch (status) {
@@ -37,10 +23,6 @@ export async function generateReportEmbed(
 	locale: string,
 	message?: Message,
 ): Promise<APIEmbed> {
-	const sql = container.resolve<Sql<any>>(kSQL);
-
-	const descParts = [];
-
 	const embed: APIEmbed = {
 		author: {
 			name: i18next.t('log.report_log.author', {
@@ -50,6 +32,7 @@ export async function generateReportEmbed(
 			icon_url: user.avatarURL()!,
 		},
 		color: statusToColor(report.status),
+		description: await generateReportLog(report, locale, message),
 		timestamp: new Date().toISOString(),
 		footer: {
 			text: i18next.t('log.report_log.footer', {
@@ -60,70 +43,11 @@ export async function generateReportEmbed(
 		},
 	};
 
-	descParts.push(
-		i18next.t('log.report_log.target', {
-			target: `\`${report.targetTag}\` (${report.targetId})`,
-			lng: locale,
-		}),
-		i18next.t('log.report_log.reason', {
-			reason: codeBlock(report.reason),
-			lng: locale,
-		}),
-	);
-
-	if (message) {
-		descParts.push(
-			i18next.t('log.report_log.message', {
-				message_link: hyperlink(i18next.t('log.report_log.message_sub', { lng: locale }), message.url),
-				// TextChannels have an custom `.toString()` method that returns the channel's mention.
-				// eslint-disable-next-line @typescript-eslint/no-base-to-string
-				channel: message.channel.toString(),
-				lng: locale,
-			}),
-		);
-	}
-
 	if (report.attachmentUrl) {
 		embed.image = {
 			url: report.attachmentUrl,
 		};
 	}
-
-	if (report.referenceId) {
-		const [reference] = await sql<[{ log_message_id: Snowflake | null }?]>`
-		select log_message_id
-		from cases
-		where guild_id = ${report.guildId}
-			and case_id = ${report.referenceId}`;
-
-		const modLogChannelId = await getGuildSetting(report.guildId, SettingsKeys.ModLogChannelId);
-
-		if (modLogChannelId && Reflect.has(reference ?? {}, 'log_message_id')) {
-			descParts.push(
-				i18next.t('log.report_log.case_reference', {
-					ref: hyperlink(
-						`#${report.referenceId}`,
-						messageLink(modLogChannelId, reference!.log_message_id!, report.guildId),
-					),
-					lng: locale,
-				}),
-			);
-		}
-	}
-
-	descParts.push(
-		'',
-		i18next.t('log.report_log.status', {
-			status: report.status,
-			lng: locale,
-		}),
-		i18next.t('log.report_log.created_at', {
-			created_at: time(dayjs().unix(), TimestampStyles.ShortDateTime),
-			lng: locale,
-		}),
-	);
-
-	embed.description = descParts.join('\n');
 
 	return embed;
 }
---
+++ packages/yuudachi/src/functions/logging/generateReportLog.ts
@@ -0,0 +1,73 @@
+import dayjs from 'dayjs';
+import { type Message, type Snowflake, codeBlock, hyperlink, time, TimestampStyles, messageLink } from 'discord.js';
+import i18next from 'i18next';
+import type { Sql } from 'postgres';
+import { container } from 'tsyringe';
+import { kSQL } from '../../tokens.js';
+import type { Report } from '../reports/createReport.js';
+import { getGuildSetting, SettingsKeys } from '../settings/getGuildSetting.js';
+
+export async function generateReportLog(report: Report, locale: string, message?: Message): Promise<string> {
+	const sql = container.resolve<Sql<any>>(kSQL);
+
+	const parts = [];
+
+	parts.push(
+		i18next.t('log.report_log.target', {
+			target: `\`${report.targetTag}\` (${report.targetId})`,
+			lng: locale,
+		}),
+		i18next.t('log.report_log.reason', {
+			reason: codeBlock(report.reason),
+			lng: locale,
+		}),
+	);
+
+	if (message) {
+		parts.push(
+			i18next.t('log.report_log.message', {
+				message_link: hyperlink(i18next.t('log.report_log.message_sub', { lng: locale }), message.url),
+				// TextChannels have an custom `.toString()` method that returns the channel's mention.
+				// eslint-disable-next-line @typescript-eslint/no-base-to-string
+				channel: message.channel.toString(),
+				lng: locale,
+			}),
+		);
+	}
+
+	if (report.referenceId) {
+		const [reference] = await sql<[{ log_message_id: Snowflake | null }?]>`
+		select log_message_id
+		from cases
+		where guild_id = ${report.guildId}
+			and case_id = ${report.referenceId}`;
+
+		const modLogChannelId = await getGuildSetting(report.guildId, SettingsKeys.ModLogChannelId);
+
+		if (modLogChannelId && Reflect.has(reference ?? {}, 'log_message_id')) {
+			parts.push(
+				i18next.t('log.report_log.case_reference', {
+					ref: hyperlink(
+						`#${report.referenceId}`,
+						messageLink(modLogChannelId, reference!.log_message_id!, report.guildId),
+					),
+					lng: locale,
+				}),
+			);
+		}
+	}
+
+	parts.push(
+		'',
+		i18next.t('log.report_log.status', {
+			status: report.status,
+			lng: locale,
+		}),
+		i18next.t('log.report_log.created_at', {
+			created_at: time(dayjs().unix(), TimestampStyles.ShortDateTime),
+			lng: locale,
+		}),
+	);
+
+	return parts.join('\n');
+}
