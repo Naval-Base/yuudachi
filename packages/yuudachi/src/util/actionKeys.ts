6f9b0972028ac66ee2781d8399360b98cabb0418
almostSouji <timoqueezle@gmail.com>
2022-09-10 17:16:15 +0200

    refactor: standardize and reuse case history embed

--- packages/yuudachi/src/util/actionKeys.ts
+++ packages/yuudachi/src/util/actionKeys.ts
@@ -1,2 +1,2 @@
-export const ACTION_KEYS = ["restriction", "", "warn", "kick", "softban", "ban", "unban", "timeout", ""];
-export const REPORT_KEYS = ["pending", "approved", "rejected", "spam"];
+export const ACTION_KEYS = ["restriction", "", "warn", "kick", "softban", "ban", "unban", "timeout", ""] as const;
+export const REPORT_KEYS = ["pending", "approved", "rejected", "spam"] as const;
--- packages/yuudachi/src/util/generateHistory.ts
+++ packages/yuudachi/src/util/generateHistory.ts
@@ -7,6 +7,7 @@ import {
 	type GuildMember,
 	type SelectMenuInteraction,
 	type User,
+	type APIEmbed,
 	hyperlink,
 	inlineCode,
 	time,
@@ -16,14 +17,15 @@ import {
 import i18next from "i18next";
 import type { Sql } from "postgres";
 import { container } from "tsyringe";
-import { Color, ThreatLevelColor } from "../Constants.js";
+import { Color, EMBED_DESCRIPTION_LIMIT, HISTORY_DESCRIPTION_MAX_LENGTH, ThreatLevelColor } from "../Constants.js";
 import type { RawCase } from "../functions/cases/transformCase.js";
 import { ReportStatus } from "../functions/reports/createReport.js";
 import type { RawReport } from "../functions/reports/transformReport.js";
 import { getGuildSetting, SettingsKeys } from "../functions/settings/getGuildSetting.js";
+import { logger } from "../logger.js";
 import { kSQL } from "../tokens.js";
 import { ACTION_KEYS, REPORT_KEYS } from "./actionKeys.js";
-import { addFields } from "./embed.js";
+import { addFields, truncate } from "./embed.js";
 
 dayjs.extend(relativeTime);
 
@@ -48,25 +50,133 @@ const colors = [
 	ThreatLevelColor.Level7,
 ];
 
+type HistoryRecord = {
+	created: string;
+	description?: string;
+	identifierLabel: string;
+	identifierURL?: string;
+	label: string;
+};
+
+function generateHistoryEmbed(
+	author: User,
+	title: string,
+	color: number,
+	records: HistoryRecord[],
+	footerText: string,
+	locale: string,
+): APIEmbed {
+	const descriptionParts = [];
+	const truncatePhrase = i18next.t("common.and_more", { lng: locale });
+
+	if (!records.length) {
+		descriptionParts.push(i18next.t("log.history.common.errors.no_history", { lng: locale }));
+	}
+
+	for (const record of records) {
+		const dateFormatted = time(dayjs(record.created).unix(), TimestampStyles.ShortDate);
+		const recordString = `${dateFormatted} ${inlineCode(record.label)} ${
+			record.identifierURL ? hyperlink(record.identifierLabel, record.identifierURL) : record.identifierLabel
+		} ${
+			record.description
+				? truncate(oneLine(record.description.replaceAll("*", "")), HISTORY_DESCRIPTION_MAX_LENGTH, "")
+				: ""
+		}`;
+
+		if (
+			descriptionParts.join("\n").length + recordString.length + 1 <
+			EMBED_DESCRIPTION_LIMIT - truncatePhrase.length
+		) {
+			descriptionParts.push(recordString);
+		} else {
+			descriptionParts.push(truncatePhrase);
+		}
+	}
+
+	return {
+		author: {
+			name: `${author.tag} (${author.id})`,
+			icon_url: author.displayAvatarURL(),
+		},
+		title,
+		color,
+		description: descriptionParts.join("\n"),
+		footer: {
+			text: footerText,
+		},
+	};
+}
+
+function actionKeyLabel(key: typeof ACTION_KEYS[number], locale: string) {
+	switch (key) {
+		case "restriction":
+			return i18next.t("log.history.cases.action_label.restriction", { lng: locale });
+		case "warn":
+			return i18next.t("log.history.cases.action_label.warn", { lng: locale });
+		case "kick":
+			return i18next.t("log.history.cases.action_label.kick", { lng: locale });
+		case "softban":
+			return i18next.t("log.history.cases.action_label.softban", { lng: locale });
+		case "ban":
+			return i18next.t("log.history.cases.action_label.ban", { lng: locale });
+		case "unban":
+			return i18next.t("log.history.cases.action_label.unban", { lng: locale });
+		case "timeout":
+			return i18next.t("log.history.cases.action_label.timeout", { lng: locale });
+		default:
+			return i18next.t("log.history.cases.action_label.unknown", { lng: locale });
+	}
+}
+
+function actionSummary(
+	restrictions: number,
+	warns: number,
+	kicks: number,
+	softbans: number,
+	bans: number,
+	unbans: number,
+	timeouts: number,
+	locale: string,
+) {
+	return [
+		i18next.t("log.history.cases.summary.restriction", {
+			count: restrictions,
+			lng: locale,
+		}),
+		i18next.t("log.history.cases.summary.warning", {
+			count: warns,
+			lng: locale,
+		}),
+		i18next.t("log.history.cases.summary.kick", {
+			count: kicks,
+			lng: locale,
+		}),
+		i18next.t("log.history.cases.summary.softban", {
+			count: softbans,
+			lng: locale,
+		}),
+		i18next.t("log.history.cases.summary.ban", {
+			count: bans,
+			lng: locale,
+		}),
+		i18next.t("log.history.cases.summary.unban", {
+			count: unbans,
+			lng: locale,
+		}),
+		i18next.t("log.history.cases.summary.timeout", {
+			count: timeouts,
+			lng: locale,
+		}),
+	].join(", ");
+}
+
 export async function generateCaseHistory(
 	interaction: ButtonInteraction<"cached"> | CommandInteraction<"cached"> | SelectMenuInteraction<"cached">,
 	target: { member?: GuildMember | undefined; user: User },
 	locale: string,
-	omitAuthor = false,
 ) {
 	const sql = container.resolve<Sql<any>>(kSQL);
-
 	const moduleLogChannelId = await getGuildSetting(interaction.guildId, SettingsKeys.ModLogChannelId);
-
-	let embed = addFields({
-		author: omitAuthor
-			? undefined
-			: {
-					name: `${target.user.tag} (${target.user.id})`,
-					icon_url: target.user.displayAvatarURL(),
-			  },
-		title: i18next.t("log.history.title", { lng: locale }),
-	});
 
 	const cases = await sql<[RawCase]>`
 		select *
@@ -77,98 +187,87 @@ export async function generateCaseHistory(
 		order by created_at desc
 	`;
 
-	const footer = cases.reduce((count: CaseFooter, case_) => {
+	const caseCounter = cases.reduce((count: CaseFooter, case_) => {
 		const action = ACTION_KEYS[case_.action]!;
 		count[action] = (count[action] ?? 0) + 1;
 		return count;
 	}, {});
 
 	const values: [number, number, number, number, number, number, number] = [
-		footer.unban ?? 0,
-		footer.warn ?? 0,
-		footer.restriction ?? 0,
-		footer.kick ?? 0,
-		footer.softban ?? 0,
-		footer.ban ?? 0,
-		footer.timeout ?? 0,
+		caseCounter.unban ?? 0,
+		caseCounter.warn ?? 0,
+		caseCounter.restriction ?? 0,
+		caseCounter.kick ?? 0,
+		caseCounter.softban ?? 0,
+		caseCounter.ban ?? 0,
+		caseCounter.timeout ?? 0,
 	];
-	const [unban, warn, restriction, kick, softban, ban, timeout] = values;
 	const colorIndex = Math.min(
 		values.reduce((a, b) => a + b),
 		colors.length - 1,
 	);
 
-	embed = {
-		color: colors[colorIndex],
-		footer: {
-			text: oneLine`${warn} warning${warn > 1 || warn === 0 ? "s" : ""},
-					${restriction} restriction${restriction > 1 || restriction === 0 ? "s" : ""},
-					${timeout} timeout${timeout > 1 || timeout === 0 ? "s" : ""},
-					${kick} kick${kick > 1 || kick === 0 ? "s" : ""},
-					${softban} softban${softban > 1 || softban === 0 ? "s" : ""},
-					${ban} ban${ban > 1 || ban === 0 ? "s" : ""},
-					${unban} unban${unban > 1 || unban === 0 ? "s" : ""}`,
-		},
-		...embed,
-	};
-
-	const summary: string[] = [];
-	let truncated = false;
-
-	for (const case_ of cases) {
-		const dateFormatted = time(dayjs(case_.created_at).unix(), TimestampStyles.ShortDate);
-		const caseString = `${dateFormatted} ${inlineCode(`${ACTION_KEYS[case_.action]!.toUpperCase()}`)} ${
-			case_.log_message_id
-				? hyperlink(`#${case_.case_id}`, messageLink(moduleLogChannelId, case_.log_message_id, case_.guild_id))
-				: `#${case_.case_id}`
-		} ${case_.reason?.replace(/\*/g, "") ?? ""}`;
-
-		if (summary.join("\n").length + caseString.length + 1 < 4_060) {
-			summary.push(caseString);
-			continue;
-		}
-
-		truncated = true;
-		break;
-	}
-
-	if (truncated) {
-		embed = {
-			description: i18next.t("log.history.summary_truncated", { summary: summary.join("\n"), lng: locale }),
-			...embed,
+	const records: HistoryRecord[] = cases.map((case_) => {
+		return {
+			created: case_.created_at,
+			identifierLabel: `#${case_.case_id}`,
+			identifierURL: case_.log_message_id
+				? messageLink(moduleLogChannelId, case_.log_message_id, case_.guild_id)
+				: undefined,
+			label: actionKeyLabel(ACTION_KEYS[case_.action]!, locale),
+			description: case_.reason ?? undefined,
 		};
-	} else {
-		embed = { description: summary.join("\n"), ...embed };
-	}
+	});
 
-	if (!embed.description?.length) {
-		embed = {
-			description: i18next.t("log.history.none", { lng: locale }),
-			...embed,
-		};
-	}
+	return generateHistoryEmbed(
+		target.user,
+		i18next.t("log.history.cases.title", { lng: locale }),
+		colors[colorIndex] ?? Color.DiscordEmbedBackground,
+		records,
+		actionSummary(...values, locale),
+		locale,
+	);
+}
 
-	return embed;
+function reportKeyLabel(key: typeof REPORT_KEYS[number], locale: string) {
+	switch (key) {
+		case "pending":
+			return i18next.t("log.history.reports.status_label.pending", { lng: locale });
+		case "approved":
+			return i18next.t("log.history.reports.status_label.approved", { lng: locale });
+		case "rejected":
+			return i18next.t("log.history.reports.status_label.rejected", { lng: locale });
+		case "spam":
+			return i18next.t("log.history.reports.status_label.spam", { lng: locale });
+		default:
+			return i18next.t("log.history.reports.status_label.unknown", { lng: locale });
+	}
+}
+
+function reportSummary(reported: number, authored: number, spam: number, locale: string) {
+	return [
+		i18next.t("log.history.reports.summary.target", {
+			count: reported,
+			lng: locale,
+		}),
+		i18next.t("log.history.reports.summary.author", {
+			count: authored,
+			lng: locale,
+		}),
+		i18next.t("log.history.reports.summary.spam", {
+			count: spam,
+			lng: locale,
+		}),
+	].join(", ");
 }
 
 export async function generateReportHistory(
 	interaction: ButtonInteraction<"cached"> | CommandInteraction<"cached"> | SelectMenuInteraction<"cached">,
 	target: { member?: GuildMember | undefined; user: User },
 	locale: string,
-	omitAuthor = false,
 ) {
 	const sql = container.resolve<Sql<any>>(kSQL);
 	const reportChannelId = await getGuildSetting(interaction.guildId, SettingsKeys.ReportChannelId);
-
-	let embed = addFields({
-		author: omitAuthor
-			? undefined
-			: {
-					name: `${target.user.tag} (${target.user.id})`,
-					icon_url: target.user.displayAvatarURL(),
-			  },
-		title: i18next.t("log.history.report_title", { lng: locale }),
-	});
 
 	const reports = await sql<[RawReport]>`
 		select *
@@ -187,57 +286,44 @@ export async function generateReportHistory(
 		colors.length - 1,
 	);
 
-	embed = {
-		color: colors[colorIndex],
-		...embed,
-	};
+	let authorCounter = 0;
+	let targetCounter = 0;
+	let authoredSpamCounter = 0;
 
-	const summary: string[] = [];
-	let truncated = false;
+	const records: HistoryRecord[] = reports.map((report) => {
+		if (report.author_id === target.user.id) {
+			authorCounter++;
 
-	for (const report of reports) {
-		const dateFormatted = time(dayjs(report.created_at).unix(), TimestampStyles.ShortDate);
+			if (report.status === ReportStatus.Spam) {
+				authoredSpamCounter++;
+			}
+		} else if (report.target_id === target.user.id) {
+			targetCounter++;
+		}
 
 		const userRoleString =
 			report.author_id === target.user.id
-				? i18next.t("log.history.user_role.author", { lng: locale })
-				: i18next.t("log.history.user_role.target", { lng: locale });
-		const reportString = `${dateFormatted} ${inlineCode(REPORT_KEYS[report.status]!.toUpperCase())} ${
-			report.log_message_id
-				? hyperlink(
-						`#${report.report_id} (${userRoleString})`,
-						messageLink(reportChannelId, report.log_message_id),
-						report.guild_id,
-				  )
-				: `#${report.report_id}`
-		} ${report.reason.replaceAll("*", "")}`;
-
-		if (summary.join("\n").length + reportString.length + 1 < 4_060) {
-			summary.push(reportString);
-			continue;
-		}
-
-		truncated = true;
-		break;
-	}
-
-	if (truncated) {
-		embed = {
-			description: i18next.t("log.history.summary_truncated", { summary: summary.join("\n"), lng: locale }),
-			...embed,
+				? i18next.t("log.history.reports.user_role.author", { lng: locale })
+				: i18next.t("log.history.reports.user_role.target", { lng: locale });
+		return {
+			created: report.created_at,
+			identifierLabel: `#${report.report_id} (${userRoleString})`,
+			identifierURL: report.log_message_id
+				? messageLink(reportChannelId, report.log_message_id, report.guild_id)
+				: undefined,
+			label: reportKeyLabel(REPORT_KEYS[report.status]!, locale),
+			description: report.reason ?? undefined,
 		};
-	} else {
-		embed = { description: summary.join("\n"), ...embed };
-	}
+	});
 
-	if (!embed.description?.length) {
-		embed = {
-			description: i18next.t("log.history.none", { lng: locale }),
-			...embed,
-		};
-	}
-
-	return embed;
+	return generateHistoryEmbed(
+		target.user,
+		i18next.t("log.history.reports.title", { lng: locale }),
+		colors[colorIndex] ?? Color.DiscordEmbedBackground,
+		records,
+		reportSummary(targetCounter, authorCounter, authoredSpamCounter, locale),
+		locale,
+	);
 }
 
 export function generateUserInfo(target: { member?: GuildMember | undefined; user: User }, locale: string) {
@@ -253,8 +339,8 @@ export function generateUserInfo(target: { member?: GuildMember | undefined; use
 			color: Color.DiscordEmbedBackground,
 		},
 		{
-			name: i18next.t("log.history.user_details", { lng: locale }),
-			value: i18next.t("log.history.user_details_description", {
+			name: i18next.t("log.history.common.user_details.title", { lng: locale }),
+			value: i18next.t("log.history.common.user_details.description", {
 				user_mention: target.user.toString(),
 				user_tag: target.user.tag,
 				user_id: target.user.id,
@@ -269,17 +355,39 @@ export function generateUserInfo(target: { member?: GuildMember | undefined; use
 		const sinceJoinFormatted = time(dayjs(target.member.joinedTimestamp).unix(), TimestampStyles.RelativeTime);
 		const joinFormatted = time(dayjs(target.member.joinedTimestamp).unix(), TimestampStyles.ShortDateTime);
 
-		embed = addFields(embed, {
-			name: i18next.t("log.history.member_details", { lng: locale }),
-			value: i18next.t("log.history.member_details_description", {
-				member_nickname: target.member.nickname
-					? inlineCode(target.member.nickname)
-					: i18next.t("log.history.member_details_no_nickname", { lng: locale }),
-				member_roles: target.member.roles.cache.map((role) => role.name).join(", "),
+		const memberDescriptionParts = [];
+		const nonDefaultRoles = target.member.roles.cache.filter((role) => role.id !== role.guild.roles.everyone.id);
+
+		if (target.member.nickname) {
+			memberDescriptionParts.push(
+				i18next.t("log.history.common.member_details.description.nickname", {
+					nickname: target.member.nickname,
+					lng: locale,
+				}),
+			);
+		}
+
+		if (nonDefaultRoles.size) {
+			memberDescriptionParts.push(
+				i18next.t("log.history.common.member_details.description.roles", {
+					count: nonDefaultRoles.size,
+					roles: nonDefaultRoles.map((role) => role.toString()),
+					lng: locale,
+				}),
+			);
+		}
+
+		memberDescriptionParts.push(
+			i18next.t("log.history.common.member_details.description.joined", {
 				joined_at: joinFormatted,
 				joined_at_since: sinceJoinFormatted,
 				lng: locale,
 			}),
+		);
+
+		embed = addFields(embed, {
+			name: i18next.t("log.history.common.member_details.title", { lng: locale }),
+			value: memberDescriptionParts.join("\n"),
 		});
 	}
 
@@ -299,16 +407,26 @@ export async function generateHistory(
 ) {
 	let embed = generateUserInfo(target, locale);
 
-	if (type === HistoryType.Case) {
-		embed = {
-			...(await generateCaseHistory(interaction, target, locale, true)),
-			...embed,
-		};
-	} else {
-		embed = {
-			...(await generateReportHistory(interaction, target, locale, true)),
-			...embed,
-		};
+	switch (type) {
+		case HistoryType.Case: {
+			embed = {
+				...(await generateCaseHistory(interaction, target, locale)),
+				...embed,
+			};
+			break;
+		}
+
+		case HistoryType.Report: {
+			embed = {
+				...(await generateReportHistory(interaction, target, locale)),
+				...embed,
+			};
+			break;
+		}
+
+		default: {
+			logger.warn(`Unhandled history type: ${HistoryType[type]} (${type})`);
+		}
 	}
 
 	if (!embed.color) {
