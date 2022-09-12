1083697e384db0884fa80fe051715f4e72918ddf
JPBM135 <jpedrobm0@gmail.com>
2022-09-11 23:12:55 -0300

    fix: lint

--- packages/yuudachi/src/functions/autocomplete/reasons.ts
+++ packages/yuudachi/src/functions/autocomplete/reasons.ts
@@ -1,6 +1,6 @@
-import type { AutocompleteInteraction } from 'discord.js';
-import i18next from 'i18next';
-import { AUTOCOMPLETE_REASON_FOCUSED_FIELD_NAME } from '../../Constants.js';
+import type { AutocompleteInteraction } from "discord.js";
+import i18next from "i18next";
+import { AUTOCOMPLETE_REASON_FOCUSED_FIELD_NAME } from "../../Constants.js";
 
 export function checkReasonAutocomplete(interaction: AutocompleteInteraction) {
 	return interaction.options.getFocused(true).name === AUTOCOMPLETE_REASON_FOCUSED_FIELD_NAME;
@@ -8,7 +8,7 @@ export function checkReasonAutocomplete(interaction: AutocompleteInteraction) {
 
 export async function handleAutocompleteReasons(interaction: AutocompleteInteraction, locale: string): Promise<void> {
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-	const reasons = i18next.t('command.mod.common.reasons', { returnObjects: true, lng: locale }) as string[];
+	const reasons = i18next.t("command.mod.common.reasons", { returnObjects: true, lng: locale }) as string[];
 
 	const mappedReasons = reasons.map((name) => ({ name, value: name }));
 
