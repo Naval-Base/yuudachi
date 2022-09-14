8ff95f8e2443ca9f1e0f7371774b1eb3938bac24
JPBM135 <jpedrobm0@gmail.com>
2022-09-13 23:12:19 -0300

    perf(update): improve performance of the update functions

--- packages/yuudachi/src/functions/reports/updateReport.ts
+++ packages/yuudachi/src/functions/reports/updateReport.ts
@@ -2,6 +2,7 @@ import type { User } from "discord.js";
 import type { Sql } from "postgres";
 import { container } from "tsyringe";
 import { kSQL } from "../../tokens.js";
+import { removeUndefinedKeys } from "../../util/object.js";
 import type { CreateReport } from "./createReport.js";
 import { type RawReport, transformReport } from "./transformReport.js";
 
@@ -13,67 +14,24 @@ export type PatchReport = Pick<
 export async function updateReport(report: PatchReport, moderator?: User) {
 	const sql = container.resolve<Sql<any>>(kSQL);
 
-	if (report.status) {
-		await sql`
-			update reports
-			set status = ${report.status}
-			where guild_id = ${report.guildId}
-				and report_id = ${report.reportId!}
-		`;
-	}
+	const queries: Partial<Record<keyof RawReport, unknown>> = {
+		status: report.status,
+		attachment_url: report.attachmentUrl,
+		reason: report.reason,
+		message_id: report.message?.id,
+		channel_id: report.message?.channel.id,
+		ref_id: report.refId,
+		mod_id: moderator?.id,
+		mod_tag: moderator?.tag,
+	};
 
-	if (report.attachmentUrl) {
-		await sql`
-			update reports
-			set attachment_url = ${report.attachmentUrl}
-			where guild_id = ${report.guildId}
-				and report_id = ${report.reportId!}
-		`;
-	}
-
-	if (report.reason) {
-		await sql`
-			update reports
-			set reason = ${report.reason}
-			where guild_id = ${report.guildId}
-				and report_id = ${report.reportId!}
-		`;
-	}
-
-	if (report.message) {
-		await sql`
-			update reports
-			set message_id = ${report.message.id},
-				channel_id = ${report.message.channel.id}
-			where guild_id = ${report.guildId}
-				and report_id = ${report.reportId!}
-		`;
-	}
-
-	if (report.refId) {
-		await sql`
-			update reports
-			set ref_id = ${report.refId}
-			where guild_id = ${report.guildId}
-				and report_id = ${report.reportId!}
-		`;
-	}
-
-	if (moderator) {
-		await sql`
-			update reports
-			set mod_id = ${moderator.id},
-				mod_tag = ${moderator.tag}
-			where guild_id = ${report.guildId}
-				and report_id = ${report.reportId!}
-		`;
-	}
+	const updates = removeUndefinedKeys(queries);
 
 	const [updatedCase] = await sql<[RawReport]>`
-		select *
-		from reports
+		update reports set ${sql(updates as Record<string, unknown>, ...Object.keys(updates))}
 		where guild_id = ${report.guildId}
 			and report_id = ${report.reportId!}
+		returning *
 	`;
 
 	return transformReport(updatedCase);
