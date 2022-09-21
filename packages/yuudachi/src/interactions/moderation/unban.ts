bd28bed44e01b6845f77e9da948fa422267d0306
JPBM135 <67063134+JPBM135@users.noreply.github.com>
2022-09-21 10:51:29 -0300

    fix(reasons): Remove autocomplete from non applicable commands (#1130)

--- packages/yuudachi/src/interactions/moderation/unban.ts
+++ packages/yuudachi/src/interactions/moderation/unban.ts
@@ -14,7 +14,6 @@ export const UnbanCommand = {
 			name: "reason",
 			description: "The reason of this action",
 			type: ApplicationCommandOptionType.String,
-			autocomplete: true,
 		},
 		{
 			name: "report_reference",
