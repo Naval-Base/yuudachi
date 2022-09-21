7702578e805d872c5d9e7107eb53a8cbb7a8e35f
almostSouji <timoqueezle@gmail.com>
2022-09-21 10:55:11 +0200

    chore: migration formatting

--- packages/yuudachi/migrations/1663584410-report-forums.js
+++ packages/yuudachi/migrations/1663584410-report-forums.js
@@ -7,8 +7,7 @@ export async function up(sql) {
 
 		alter table reports
 			rename column log_message_id to log_post_id;
-		
-			
+
 		comment on column guild_settings.report_status_tags is 'The forum labels to use to sync status [pending, approved, rejected, spam]';
 		comment on column guild_settings.report_type_tags is 'The forum labels to show report types [message_report, user_report]';
 		comment on column reports.log_post_id is 'The id of the report ticket post';
