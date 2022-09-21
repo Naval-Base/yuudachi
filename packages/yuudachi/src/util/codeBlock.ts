446d4ce434374a0321652a9bfae0c8c908ea1dfd
JPBM135 <jpedrobm0@gmail.com>
2022-09-21 16:22:10 -0300

    perf(codeBlocks): improve regex

--- packages/yuudachi/src/util/codeBlock.ts
+++ packages/yuudachi/src/util/codeBlock.ts
@@ -1,4 +1,3 @@
 export function removeCodeBlocks(input: string, replace = "") {
-	// eslint-disable-next-line prefer-named-capture-group -- Needs to be capturing because of the back-reference
-	return input.replaceAll(/(`{1,3})(?<codeblock>.*?)(\1)/gs, replace);
+	return input.replaceAll(/(?<q>`{1,3}).*?\1/gs, replace);
 }
