3e390fb58122337024ee807a411f6fe9f2dbd8f4
JPBM135 <67063134+JPBM135@users.noreply.github.com>
2022-10-08 22:56:57 -0300

    feat(reasons): Use option filtering to improve reasons autocomplete UX (#1135)
    Co-authored-by: iCrawl <buechler.noel@outlook.com>

--- packages/yuudachi/src/functions/autocomplete/reasons.ts
+++ packages/yuudachi/src/functions/autocomplete/reasons.ts
@@ -1,16 +1,21 @@
 import type { AutocompleteInteraction } from "discord.js";
 import i18next from "i18next";
 import { AUTOCOMPLETE_REASON_FOCUSED_FIELD_NAME } from "../../Constants.js";
+import { findBestMatch } from "../../util/findBestMatch.js";
 
 export function checkReasonAutocomplete(interaction: AutocompleteInteraction) {
 	return interaction.options.getFocused(true).name === AUTOCOMPLETE_REASON_FOCUSED_FIELD_NAME;
 }
 
 export async function handleAutocompleteReasons(interaction: AutocompleteInteraction, locale: string): Promise<void> {
+	const input = interaction.options.getString(AUTOCOMPLETE_REASON_FOCUSED_FIELD_NAME, true);
+
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
 	const reasons = i18next.t("command.mod.common.reasons", { returnObjects: true, lng: locale }) as string[];
 
-	const mappedReasons = reasons.map((name) => ({ name, value: name }));
+	const matches = findBestMatch(input, reasons);
+
+	const mappedReasons = matches.map((name) => ({ name, value: name }));
 
 	await interaction.respond(mappedReasons);
 }
