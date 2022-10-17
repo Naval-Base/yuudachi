c7470bb4edd9d536bf6077aa3ff864e031f91335
JPBM135 <jpedrobm0@gmail.com>
2022-10-17 00:06:48 -0300

    feat(reports): util function to resolve pending reports

---
+++ packages/yuudachi/src/functions/reports/resolveReports.ts
@@ -0,0 +1,51 @@
+import { kSQL, logger } from "@yuudachi/framework";
+import type { Guild, User } from "discord.js";
+import type { Sql } from "postgres";
+import { container } from "tsyringe";
+import { upsertReportLog } from "../logging/upsertReportLog.js";
+import { ReportStatus } from "./createReport.js";
+import type { RawReport } from "./transformReport.js";
+import { updateReport } from "./updateReport.js";
+
+export async function resolvePendingReports(guild: Guild, targetId: string, caseId: number, moderator: User) {
+	const sql = container.resolve<Sql<any>>(kSQL);
+
+	const pendingReports = await sql<RawReport[]>`
+		select *
+		from reports
+		where guild_id = ${guild.id}
+			and status = ${ReportStatus.Pending}
+			and (target_id = ${targetId} or author_id = ${targetId})
+		order by created_at asc
+	`;
+
+	for (const report of pendingReports) {
+		try {
+			const status = report.target_id === targetId ? ReportStatus.Approved : ReportStatus.Spam;
+
+			const updatedReport = await updateReport(
+				{
+					guildId: guild.id,
+					reportId: report.report_id,
+					refId: caseId,
+					status,
+				},
+				moderator,
+			);
+
+			await upsertReportLog(guild, updatedReport);
+		} catch (error) {
+			logger.error(
+				{
+					error,
+					reportId: report.report_id,
+					targetId,
+					moderatorId: moderator.id,
+				},
+				"Failed to automatically resolve report ",
+			);
+		}
+	}
+
+	return pendingReports;
+}
