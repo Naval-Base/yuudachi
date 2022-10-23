90a2228dc5bfc4a33d31607031d316e26e7d3cde
JPBM135 <jpedrobm0@gmail.com>
2022-10-23 10:09:11 -0300

    framework/feat(type): Type for posting commands to the API

--- packages/framework/src/types/Command.ts
+++ packages/framework/src/types/Command.ts
@@ -1,3 +1,10 @@
+import type { APIApplicationCommand } from "discord-api-types/v10";
+
 export type CommandInfo = {
+	name: string;
+};
+
+export type RESTPostAPIApplicationCommand = Partial<Omit<APIApplicationCommand, "description" | "name">> & {
+	description: string;
 	name: string;
 };
