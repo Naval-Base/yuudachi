495f4cdaad571f5d49f8597cd208cc8397a4502f
JPBM135 <jpedrobm0@gmail.com>
2022-10-23 15:45:05 -0300

    refactor(commands): localize commands

--- packages/yuudachi/src/deploy.ts
+++ packages/yuudachi/src/deploy.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import process from "node:process";
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v10";
@@ -36,48 +37,55 @@ import {
 	ReportMessageContextCommand,
 	ReportUserContextCommand,
 } from "./interactions/index.js";
+import { formatCommandToLocalizations } from "./interactions/resolveLocales.js";
+import { createI18next } from "./util/i18next.js";
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN!);
+
+await createI18next();
 
 try {
 	console.log("Start refreshing interaction (/) commands.");
 
+	const moderation = [
+		AntiRaidNukeCommand,
+		BanCommand,
+		CaseLookupCommand,
+		DurationCommand,
+		HistoryCommand,
+		KickCommand,
+		ReasonCommand,
+		ReferenceCommand,
+		RestrictCommand,
+		SoftbanCommand,
+		UnbanCommand,
+		WarnCommand,
+		LockdownCommand,
+		TimeoutCommand,
+		ClearCommand,
+		ReportUtilsCommand,
+	].map((command) => formatCommandToLocalizations("moderation", command as any));
+
+	const utility = [
+		PingCommand,
+		CheckScamCommand,
+		RefreshScamlistCommand,
+		SponsorCommand,
+		RepostCommand,
+		ReportCommand,
+	].map((command) => formatCommandToLocalizations("utility", command as any));
+
+	const contextMenu = [
+		HistoryUserContextCommand,
+		SponsorUserContextCommand,
+		RepostMessageContextCommand,
+		ClearContextCommand,
+		ReportMessageContextCommand,
+		ReportUserContextCommand,
+	].map((command) => formatCommandToLocalizations("context-menu", command as any));
+
 	await rest.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID!, process.env.DISCORD_GUILD_ID!), {
-		body: [
-			// Moderation
-			AntiRaidNukeCommand,
-			BanCommand,
-			CaseLookupCommand,
-			DurationCommand,
-			HistoryCommand,
-			KickCommand,
-			ReasonCommand,
-			ReferenceCommand,
-			RestrictCommand,
-			SoftbanCommand,
-			UnbanCommand,
-			WarnCommand,
-			LockdownCommand,
-			TimeoutCommand,
-			ClearCommand,
-			ReportUtilsCommand,
-
-			// Utility
-			PingCommand,
-			CheckScamCommand,
-			RefreshScamlistCommand,
-			SponsorCommand,
-			RepostCommand,
-			ReportCommand,
-
-			// Context Menu
-			HistoryUserContextCommand,
-			SponsorUserContextCommand,
-			RepostMessageContextCommand,
-			ClearContextCommand,
-			ReportMessageContextCommand,
-			ReportUserContextCommand,
-		],
+		body: [...moderation, ...utility, ...contextMenu],
 	});
 	console.log("Successfully reloaded interaction (/) commands.");
 } catch (error) {
