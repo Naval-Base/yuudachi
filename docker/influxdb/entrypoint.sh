17efaf30906d04aa991f28172d415e049f546c8b
iCrawl <buechler.noel@outlook.com>
2022-12-17 01:14:06 +0100

    fix: influx setup

--- docker/influxdb/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -eu -o pipefail
-
-influx setup --skip-verify --bucket $INFLUXDB_INIT_BUCKET --retention $INFLUXDB_INIT_RETENTION --token $INFLUXDB_INIT_ADMIN_TOKEN --org $INFLUXDB_INIT_ORG --username $INFLUXDB_INIT_USERNAME --password $INFLUXDB_INIT_PASSWORD --host http://$INFLUXDB_INIT_HOST:8086 --force
