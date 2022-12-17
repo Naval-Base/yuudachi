b36584dc9f978b9c9f3246949960e2155f685924
iCrawl <buechler.noel@outlook.com>
2022-12-17 17:02:25 +0100

    fix: allow old discordapp domain

--- packages/yuudachi/src/util/resolveMessage.ts
+++ packages/yuudachi/src/util/resolveMessage.ts
@@ -5,7 +5,7 @@ import { getGuildSetting, SettingsKeys } from "../functions/settings/getGuildSet
 
 export function parseMessageLink(link: string) {
 	const linkRegex =
-		/(?:https?:\/\/(?:ptb\.|canary\.)?discord\.com\/channels\/(?<guildId>\d{17,20})\/(?<channelId>\d{17,20})\/(?<messageId>\d{17,20}))/gi;
+		/(?:https?:\/\/(?:ptb\.|canary\.)?discord(?:app)?\.com\/channels\/(?<guildId>\d{17,20})\/(?<channelId>\d{17,20})\/(?<messageId>\d{17,20}))/gi;
 	const groups = linkRegex.exec(link)?.groups;
 
 	if (!groups) {
