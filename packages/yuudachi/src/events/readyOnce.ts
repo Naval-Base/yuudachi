893f0548f2422b20bfccfa4b519bab4b7485f593
iCrawl <buechler.noel@outlook.com>
2022-12-18 19:41:34 +0100

    feat: add total member stats

--- packages/yuudachi/src/events/readyOnce.ts
+++ packages/yuudachi/src/events/readyOnce.ts
@@ -19,6 +19,19 @@ new Gauge({
 	},
 });
 
+new Gauge({
+	name: "yuudachi_bot_v3_members_total",
+	help: "Total users",
+	labelNames: ["guildId"],
+	collect() {
+		const client = container.resolve<Client<true>>(Client);
+
+		for (const [, guild] of client.guilds.cache) {
+			this.set({ guildId: guild.id }, guild.memberCount);
+		}
+	},
+});
+
 @injectable()
 export default class implements Event {
 	public name = "Client ready once handling";
