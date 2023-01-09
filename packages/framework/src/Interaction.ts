96a0f81562f9d69abe4943a5d0e736a10c10712a
iCrawl <buechler.noel@outlook.com>
2023-01-09 23:38:15 +0100

    feat: expand transformation

--- packages/framework/src/Interaction.ts
+++ packages/framework/src/Interaction.ts
@@ -5,6 +5,11 @@ import type {
 	Permissions,
 	APIAttachment,
 	Snowflake,
+	APIApplicationCommandInteractionDataOption,
+	APIInteractionDataResolved,
+	APIInteractionDataResolvedChannel,
+	APIUser,
+	APIInteractionDataResolvedGuildMember,
 } from "discord-api-types/v10";
 import { InteractionType, ComponentType } from "discord-api-types/v10";
 import type {
@@ -26,10 +31,92 @@ import {
 } from "discord.js";
 import type { ArgumentsOf, CommandPayload, ComponentPayload, Runtime } from "./types/ArgumentsOf.js";
 
+export type Option = {
+	attachment?: APIAttachment;
+	channel?: APIInteractionDataResolvedChannel;
+	member?: APIInteractionDataResolvedGuildMember;
+	name: string;
+	role?: APIRole;
+	type: number;
+	user?: APIUser;
+	value?: boolean | number | string;
+};
+
+export type TransformResult = Option & {
+	options?: Option[];
+};
+
+export function transformOption(
+	option: APIApplicationCommandInteractionDataOption,
+	resolved: APIInteractionDataResolved,
+) {
+	const result: TransformResult = {
+		name: option.name,
+		type: option.type,
+	};
+
+	switch (option.type) {
+		case ApplicationCommandOptionType.Subcommand:
+		case ApplicationCommandOptionType.SubcommandGroup: {
+			if ("options" in option) {
+				result.options = option.options?.map((opt) => transformOption(opt, resolved));
+			}
+
+			break;
+		}
+
+		case ApplicationCommandOptionType.Boolean:
+		case ApplicationCommandOptionType.Integer:
+		case ApplicationCommandOptionType.Number:
+		case ApplicationCommandOptionType.String: {
+			if ("value" in option) {
+				result.value = option.value;
+			}
+
+			break;
+		}
+
+		case ApplicationCommandOptionType.Attachment:
+		case ApplicationCommandOptionType.Channel:
+		case ApplicationCommandOptionType.Mentionable:
+		case ApplicationCommandOptionType.Role:
+		case ApplicationCommandOptionType.User: {
+			if (resolved) {
+				const attachment = resolved.attachments?.[option.value];
+				if (attachment) {
+					result.attachment = attachment;
+				}
+
+				const channel = resolved.channels?.[option.value];
+				if (channel) {
+					result.channel = channel;
+				}
+
+				const role = resolved.roles?.[option.value];
+				if (role) {
+					result.role = role;
+				}
+
+				const user = resolved.users?.[option.value];
+				if (user) {
+					result.user = user;
+				}
+
+				const member = resolved.members?.[option.value];
+				if (member) {
+					result.member = member;
+				}
+			}
+		}
+	}
+
+	return result;
+}
+
 export function transformApplicationInteraction<
 	T extends CommandPayload = CommandPayload,
 	R extends Runtime = Runtime.Discordjs,
->(options: readonly CommandInteractionOption<"cached">[]): ArgumentsOf<T, R> {
+>(options: readonly CommandInteractionOption<"cached">[] | readonly TransformResult[]): ArgumentsOf<T, R> {
 	const opts: Record<
 		string,
 		| APIAttachment
@@ -42,9 +129,10 @@ export function transformApplicationInteraction<
 		| boolean
 		| number
 		| string
-		| { member?: GuildMember | undefined; user?: User | undefined }
-		| { user: APIGuildMember & { permissions: Permissions } }
-		| { user: APIGuildMember & { permissons: Permissions } }
+		| {
+				member?: GuildMember | (APIInteractionDataResolvedGuildMember & { permissions: Permissions }) | undefined;
+				user?: APIUser | User | undefined;
+		  }
 		| (APIPartialChannel & { permissions: Permissions })
 		| undefined
 	> = {};
@@ -53,6 +141,7 @@ export function transformApplicationInteraction<
 		switch (top.type) {
 			case ApplicationCommandOptionType.Subcommand:
 			case ApplicationCommandOptionType.SubcommandGroup:
+				// @ts-expect-error: This is intended
 				opts[top.name] = transformApplicationInteraction<T, R>(top.options ? [...top.options] : []);
 				break;
 			case ApplicationCommandOptionType.User:
@@ -78,6 +167,7 @@ export function transformApplicationInteraction<
 				break;
 			// @ts-expect-error: This is actually a string
 			case "_MESSAGE":
+				// @ts-expect-error: This is intended
 				opts[top.name] = top.message;
 				break;
 			default:
