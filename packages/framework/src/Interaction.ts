1739f07efa0980f97b9bdd773ab8c20816608bb7
iCrawl <buechler.noel@outlook.com>
2023-01-10 01:18:12 +0100

    refactor: raw transformers

--- packages/framework/src/Interaction.ts
+++ packages/framework/src/Interaction.ts
@@ -1,5 +1,4 @@
 import type {
-	APIGuildMember,
 	APIPartialChannel,
 	APIRole,
 	Permissions,
@@ -10,6 +9,10 @@ import type {
 	APIInteractionDataResolvedChannel,
 	APIUser,
 	APIInteractionDataResolvedGuildMember,
+	APIMessageComponentSelectMenuInteraction,
+	APIMessageComponentButtonInteraction,
+	APIModalSubmitInteraction,
+	APIGuildMember,
 } from "discord-api-types/v10";
 import { InteractionType, ComponentType } from "discord-api-types/v10";
 import type {
@@ -46,7 +49,7 @@ export type TransformResult = Option & {
 	options?: Option[];
 };
 
-export function transformOption(
+export function transformCommandOption(
 	option: APIApplicationCommandInteractionDataOption,
 	resolved: APIInteractionDataResolved,
 ) {
@@ -59,7 +62,7 @@ export function transformOption(
 		case ApplicationCommandOptionType.Subcommand:
 		case ApplicationCommandOptionType.SubcommandGroup: {
 			if ("options" in option) {
-				result.options = option.options?.map((opt) => transformOption(opt, resolved));
+				result.options = option.options?.map((opt) => transformCommandOption(opt, resolved));
 			}
 
 			break;
@@ -116,7 +119,7 @@ export function transformOption(
 export function transformApplicationInteraction<
 	T extends CommandPayload = CommandPayload,
 	R extends Runtime = Runtime.Discordjs,
->(options: readonly CommandInteractionOption<"cached">[] | readonly TransformResult[]): ArgumentsOf<T, R> {
+>(options: readonly CommandInteractionOption<"cached">[]): ArgumentsOf<T, R> {
 	const opts: Record<
 		string,
 		| APIAttachment
@@ -130,7 +133,7 @@ export function transformApplicationInteraction<
 		| number
 		| string
 		| {
-				member?: GuildMember | (APIInteractionDataResolvedGuildMember & { permissions: Permissions }) | undefined;
+				member?: GuildMember | (APIGuildMember & { permissions: Permissions }) | undefined;
 				user?: APIUser | User | undefined;
 		  }
 		| (APIPartialChannel & { permissions: Permissions })
@@ -141,7 +144,6 @@ export function transformApplicationInteraction<
 		switch (top.type) {
 			case ApplicationCommandOptionType.Subcommand:
 			case ApplicationCommandOptionType.SubcommandGroup:
-				// @ts-expect-error: This is intended
 				opts[top.name] = transformApplicationInteraction<T, R>(top.options ? [...top.options] : []);
 				break;
 			case ApplicationCommandOptionType.User:
@@ -167,8 +169,62 @@ export function transformApplicationInteraction<
 				break;
 			// @ts-expect-error: This is actually a string
 			case "_MESSAGE":
-				// @ts-expect-error: This is intended
 				opts[top.name] = top.message;
+				break;
+			default:
+				break;
+		}
+	}
+
+	return opts as ArgumentsOf<T, R>;
+}
+
+export function transformApplicationInteractionRaw<
+	T extends CommandPayload = CommandPayload,
+	R extends Runtime = Runtime.Raw,
+>(options: readonly TransformResult[]): ArgumentsOf<T, R> {
+	const opts: Record<
+		string,
+		| APIAttachment
+		| APIInteractionDataResolvedChannel
+		| APIRole
+		| ArgumentsOf<T, R>
+		| boolean
+		| number
+		| string
+		| {
+				member?: APIInteractionDataResolvedGuildMember | undefined;
+				user?: APIUser | undefined;
+		  }
+		| undefined
+	> = {};
+
+	for (const top of options) {
+		switch (top.type) {
+			case ApplicationCommandOptionType.Subcommand:
+			case ApplicationCommandOptionType.SubcommandGroup:
+				opts[top.name] = transformApplicationInteractionRaw<T, R>(top.options ? [...top.options] : []);
+				break;
+			case ApplicationCommandOptionType.User:
+				opts[top.name] = { user: top.user, member: top.member };
+				break;
+			case ApplicationCommandOptionType.Channel:
+				opts[top.name] = top.channel;
+				break;
+			case ApplicationCommandOptionType.Role:
+				opts[top.name] = top.role;
+				break;
+			case ApplicationCommandOptionType.Mentionable:
+				opts[top.name] = top.user ? { user: top.user, member: top.member } : top.role;
+				break;
+			case ApplicationCommandOptionType.Number:
+			case ApplicationCommandOptionType.Integer:
+			case ApplicationCommandOptionType.String:
+			case ApplicationCommandOptionType.Boolean:
+				opts[top.name] = top.value;
+				break;
+			case ApplicationCommandOptionType.Attachment:
+				opts[top.name] = top.attachment;
 				break;
 			default:
 				break;
@@ -192,9 +248,7 @@ export function transformComponentInteraction<
 		| Collection<Snowflake, Role>
 		| string[]
 		| string
-		| { channels: (APIPartialChannel & { permissions: Permissions })[] }
 		| { members: Collection<Snowflake, GuildMember>; users: Collection<Snowflake, User> }
-		| { users: (APIGuildMember & { permissions: Permissions })[] }
 		| undefined
 	> = {};
 
@@ -239,6 +293,94 @@ export function transformComponentInteraction<
 				for (const comp of component.components) {
 					if (comp.type === ComponentType.TextInput) {
 						acc.set(comp.customId, comp.value);
+					}
+				}
+
+				return acc;
+			}, new Map<string, string>());
+
+			for (const [customId, value] of fields) {
+				opts[customId] = value;
+			}
+
+			break;
+		}
+
+		default:
+			break;
+	}
+
+	return opts as ArgumentsOf<T, R>;
+}
+
+export function transformComponentInteractionRaw<
+	T extends ComponentPayload = ComponentPayload,
+	R extends Runtime = Runtime.Discordjs,
+>(
+	interaction:
+		| APIMessageComponentButtonInteraction
+		| APIMessageComponentSelectMenuInteraction
+		| APIModalSubmitInteraction,
+): ArgumentsOf<T, R> {
+	const opts: Record<
+		string,
+		| ArgumentsOf<T, R>
+		| Map<Snowflake, APIInteractionDataResolvedChannel>
+		| Map<Snowflake, APIRole>
+		| string[]
+		| string
+		| { members: Map<Snowflake, APIInteractionDataResolvedGuildMember>; users: Map<Snowflake, APIUser> }
+		| undefined
+	> = {};
+
+	const messageComponentType = (
+		messageComponentInteraction: APIMessageComponentButtonInteraction | APIMessageComponentSelectMenuInteraction,
+	) => {
+		switch (messageComponentInteraction.data.component_type) {
+			case ComponentType.Button:
+				opts[messageComponentInteraction.data.custom_id] = messageComponentInteraction.data.custom_id;
+				break;
+			case ComponentType.StringSelect:
+				opts[messageComponentInteraction.data.custom_id] = messageComponentInteraction.data.values;
+				break;
+			case ComponentType.UserSelect:
+				opts[messageComponentInteraction.data.custom_id] = {
+					users: new Map(Object.entries(messageComponentInteraction.data.resolved.users)),
+					members: new Map(Object.entries(messageComponentInteraction.data.resolved.members ?? {})),
+				};
+				break;
+			case ComponentType.ChannelSelect:
+				opts[messageComponentInteraction.data.custom_id] = new Map(
+					Object.entries(messageComponentInteraction.data.resolved.channels),
+				);
+				break;
+			case ComponentType.RoleSelect:
+				opts[messageComponentInteraction.data.custom_id] = new Map(
+					Object.entries(messageComponentInteraction.data.resolved.roles),
+				);
+				break;
+			case ComponentType.MentionableSelect:
+				opts[messageComponentInteraction.data.custom_id] = messageComponentInteraction.data.resolved.users
+					? {
+							users: new Map(Object.entries(messageComponentInteraction.data.resolved.users ?? {})),
+							members: new Map(Object.entries(messageComponentInteraction.data.resolved.members ?? {})),
+					  }
+					: new Map(Object.entries(messageComponentInteraction.data.resolved.roles ?? {}));
+				break;
+			default:
+				break;
+		}
+	};
+
+	switch (interaction.type) {
+		case InteractionType.MessageComponent:
+			messageComponentType(interaction);
+			break;
+		case InteractionType.ModalSubmit: {
+			const fields = interaction.data.components.reduce((acc, component) => {
+				for (const comp of component.components) {
+					if (comp.type === ComponentType.TextInput) {
+						acc.set(comp.custom_id, comp.value);
 					}
 				}
 
