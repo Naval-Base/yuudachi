e59583e43c8d4560dcbe064942529f8c2acb1fd9
João Pedro Borges Martins <jpedrobm0@gmail.com>
2023-01-11 14:37:07 -0300

    chore(acquireLock): remove unused override argument

--- apps/yuudachi/src/functions/locks/locks.ts
+++ apps/yuudachi/src/functions/locks/locks.ts
@@ -5,14 +5,14 @@ import i18next from "i18next";
 import type { Redis } from "ioredis";
 import { MEMBER_LOCK_EXPIRE_SECONDS } from "../../Constants.js";
 
-export async function acquireMemberLock(member: GuildMember, locale: LocaleParam, override = false): Promise<void> {
+export async function acquireMemberLock(member: GuildMember, locale: LocaleParam): Promise<void> {
 	const redis = container.resolve<Redis>(kRedis);
 
 	const lockKey = `member-lock:${member.guild.id}:${member.id}`;
 
 	const lock = await redis.set(lockKey, member.user.createdTimestamp, "EX", MEMBER_LOCK_EXPIRE_SECONDS, "NX");
 
-	if (!lock && !override) {
+	if (!lock) {
 		throw new Error(
 			i18next.t("command.common.errors.member_lock_acquired", {
 				lng: locale,
