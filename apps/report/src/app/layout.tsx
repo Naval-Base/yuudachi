ce1053489f78a07a1973b25eb8f3fcd6dfcc450a
iCrawl <buechler.noel@outlook.com>
2023-09-30 00:20:03 +0200

    fix: re-add css reset

--- apps/report/src/app/layout.tsx
+++ apps/report/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
 
+import "@unocss/reset/normalize.css";
 import "~/styles/main.css";
 
 export const metadata: Metadata = {
