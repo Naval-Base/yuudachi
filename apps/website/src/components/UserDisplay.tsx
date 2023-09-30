dbe9d14cdac26cec1dcca0e1d930797de3dd0214
iCrawl <buechler.noel@outlook.com>
2023-09-30 03:48:14 +0200

    fix: check if banner or avatar is animated

--- apps/website/src/components/UserDisplay.tsx
+++ apps/website/src/components/UserDisplay.tsx
@@ -3,6 +3,9 @@ export function UserDisplay({
 }: {
 	readonly user: { avatar: string; banner: string; id: string; username: string };
 }) {
+	const isAvatarAnimated = user.avatar.startsWith("a_");
+	const isBannerAnimated = user.banner.startsWith("a_");
+
 	return (
 		<div className="flex w-[340px] flex-col p-5">
 			<div className="relative">
@@ -10,14 +13,16 @@ export function UserDisplay({
 					<img
 						alt="Avatar"
 						className="h-full max-h-[120px] w-full max-w-[340px] rounded-lg object-cover"
-						src={`https://cdn.discordapp.com/banners/${user.id}/${user.banner}.gif?size=480`}
+						src={`https://cdn.discordapp.com/banners/${user.id}/${user.banner}${
+							isBannerAnimated ? ".gif" : ".png"
+						}?size=480`}
 					/>
 				</picture>
 				<picture>
 					<img
 						alt="Banner"
 						className="border-3 absolute left-[22px] top-[72px] h-[80px] w-[80px] rounded-full"
-						src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
+						src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}${isAvatarAnimated ? ".gif" : ".png"}`}
 					/>
 				</picture>
 			</div>
